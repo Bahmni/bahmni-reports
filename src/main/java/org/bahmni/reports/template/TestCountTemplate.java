73d2fe816f2a6d59fe47b456dffacd330aecbec2
rohanpoddar <rohan.poddar@gmail.com>
2015-03-05 18:55:01 +0530

    Rohan, Vikash | #1799 | Add lab result count report.

--- src/main/java/org/bahmni/reports/template/TestCountTemplate.java
+++ src/main/java/org/bahmni/reports/template/TestCountTemplate.java
@@ -1,13 +1,11 @@
 package org.bahmni.reports.template;
 
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
-import net.sf.dynamicreports.report.builder.DynamicReports;
-import net.sf.dynamicreports.report.builder.crosstab.CrosstabBuilder;
-import net.sf.dynamicreports.report.builder.crosstab.CrosstabColumnGroupBuilder;
-import net.sf.dynamicreports.report.builder.crosstab.CrosstabRowGroupBuilder;
+import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
+import net.sf.dynamicreports.report.builder.group.ColumnGroupBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
-import net.sf.dynamicreports.report.builder.style.Styles;
-import net.sf.dynamicreports.report.constant.Calculation;
+import net.sf.dynamicreports.report.constant.GroupHeaderLayout;
+import net.sf.dynamicreports.report.constant.HorizontalAlignment;
 import net.sf.dynamicreports.report.constant.PageOrientation;
 import net.sf.dynamicreports.report.constant.PageType;
 import org.bahmni.reports.model.ReportConfig;
@@ -17,7 +15,11 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.*;
+import static net.sf.dynamicreports.report.builder.DynamicReports.col;
+import static net.sf.dynamicreports.report.builder.DynamicReports.grp;
+import static net.sf.dynamicreports.report.builder.DynamicReports.report;
+import static net.sf.dynamicreports.report.builder.DynamicReports.stl;
+import static net.sf.dynamicreports.report.builder.DynamicReports.type;
 import static org.bahmni.reports.util.FileReaderUtil.getFileContent;
 
 @Component(value = "TestCount")
@@ -28,36 +30,40 @@ public class TestCountTemplate implements BaseReportTemplate {
 
     @Override
     public JasperReportBuilder build(ReportConfig reportConfig, String startDate, String endDate) throws SQLException {
-        CrosstabRowGroupBuilder<String> rowGroup = ctab.rowGroup("department", String.class)
-                .setShowTotal(false);
-        CrosstabColumnGroupBuilder<String> columnGroup = ctab.columnGroup("test", String.class)
-                .setShowTotal(false);
 
-        CrosstabBuilder crosstab = ctab.crosstab()
-                .headerCell(DynamicReports.cmp.text("Age Group / Outcome"))
-                .rowGroups(rowGroup)
-                .columnGroups(columnGroup)
-                .measures(
-                        ctab.measure("Total Count", "total_count", Integer.class, Calculation.NOTHING),
-                        ctab.measure("Positive", "positive", Integer.class, Calculation.NOTHING),
-                        ctab.measure("Negative", "negative", Integer.class, Calculation.NOTHING)
-                )
-                .setCellStyle(Templates.columnStyle.setBorder(Styles.pen()));
+        StyleBuilder columnStyle = stl.style().setRightBorder(stl.pen1Point());
 
-        StyleBuilder textStyle = stl.style(Templates.columnStyle).setBorder(stl.pen1Point());
+        TextColumnBuilder<String> departmentColumn = col.column("Department", "department", type.stringType())
+                .setStyle(columnStyle);
+        TextColumnBuilder<String> testColumn = col.column("Test", "test", type.stringType())
+                .setStyle(columnStyle);
+        TextColumnBuilder<Integer> totalCountColumn = col.column("Total Count", "total_count", type.integerType())
+                .setStyle(columnStyle)
+                .setHorizontalAlignment(HorizontalAlignment.CENTER);
+        TextColumnBuilder<Integer> positiveCountColumn = col.column("Positive Count", "positive", type.integerType())
+                .setStyle(columnStyle)
+                .setHorizontalAlignment(HorizontalAlignment.CENTER);
+        TextColumnBuilder<Integer> negativeCountColumn = col.column("Negative Count", "negative", type.integerType())
+                .setHorizontalAlignment(HorizontalAlignment.CENTER);
+
+        ColumnGroupBuilder departmentGroup = grp.group(departmentColumn)
+                .setTitleWidth(30)
+                .setHeaderLayout(GroupHeaderLayout.VALUE)
+                .showColumnHeaderAndFooter()
+                .setPadding(30);
 
         String sql = getFileContent("sql/testCount.sql");
 
         JasperReportBuilder report = report();
         report.setPageFormat(PageType.A3, PageOrientation.LANDSCAPE)
-                .setColumnStyle(textStyle)
                 .setTemplate(Templates.reportTemplate)
+                .setShowColumnTitle(false)
+                .columns(departmentColumn, testColumn, totalCountColumn, positiveCountColumn, negativeCountColumn)
+                .groupBy(departmentGroup)
                 .setReportName(reportConfig.getName())
-                .summary(crosstab)
                 .pageFooter(Templates.footerComponent)
                 .setDataSource(String.format(sql, startDate, endDate),
                         openelisDataSource.getConnection());
         return report;
     }
-
 }
--- src/main/resources/sql/testCount.sql
+++ src/main/resources/sql/testCount.sql
@@ -15,12 +15,12 @@ SELECT DISTINCT
 FROM test_section ts
   INNER JOIN test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
   LEFT OUTER JOIN analysis a ON t.id = a.test_id
-  LEFT OUTER JOIN result r ON a.id = r.analysis_id
-  LEFT OUTER JOIN result r1 ON a.id = r1.analysis_id AND r1.result_type = 'D'
+  LEFT OUTER JOIN result r ON a.id = r.analysis_id and r.lastupdated BETWEEN '%s' and '%s'
+  LEFT OUTER JOIN result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id
   LEFT OUTER JOIN test_result tr ON tr.test_id = t.id AND tr.tst_rslt_type = 'D' AND tr.id = r1.test_result_id
   LEFT OUTER JOIN dictionary d ON d.id = r1.value :: INTEGER AND d.dict_entry LIKE '%%positive%%' OR
                                   d.dict_entry LIKE '%%+%%' AND tr.value :: INTEGER = d.id
   LEFT OUTER JOIN dictionary d1 ON d1.id = r1.value :: INTEGER AND d1.dict_entry LIKE '%%negative%%' OR
                                    d1.dict_entry LIKE '%%-%%' AND tr.value :: INTEGER = d1.id
-where r.lastupdated  BETWEEN '%s' and '%s'
-GROUP BY ts.name, t.name, t.id;
+GROUP BY ts.name, t.name, t.id
+order by ts.name;
