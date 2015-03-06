a99ecacd0dc72b43cda55eea2e5d98168deb57ce
rohanpoddar <rohan.poddar@gmail.com>
2015-03-06 15:49:32 +0530

    Rohan, Sravanthi | #1830 | Fixing the sort order of age groups, fixed the range, and added Other gender to the report

--- src/main/java/org/bahmni/reports/template/ObsCountByGenderAndAgeGroupTemplate.java
+++ src/main/java/org/bahmni/reports/template/ObsCountByGenderAndAgeGroupTemplate.java
@@ -8,6 +8,7 @@ import net.sf.dynamicreports.report.builder.crosstab.CrosstabRowGroupBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
 import net.sf.dynamicreports.report.builder.style.Styles;
 import net.sf.dynamicreports.report.constant.Calculation;
+import net.sf.dynamicreports.report.constant.OrderType;
 import net.sf.dynamicreports.report.constant.PageOrientation;
 import net.sf.dynamicreports.report.constant.PageType;
 import org.bahmni.reports.model.ReportConfig;
@@ -16,7 +17,9 @@ import org.springframework.stereotype.Component;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.*;
+import static net.sf.dynamicreports.report.builder.DynamicReports.ctab;
+import static net.sf.dynamicreports.report.builder.DynamicReports.report;
+import static net.sf.dynamicreports.report.builder.DynamicReports.stl;
 import static org.bahmni.reports.util.FileReaderUtil.getFileContent;
 
 @Component(value = "ObsCountByGenderAndAge")
@@ -27,16 +30,22 @@ public class ObsCountByGenderAndAgeGroupTemplate extends AbstractMRSReportTempla
         CrosstabRowGroupBuilder<String> rowGroup = ctab.rowGroup("age_group", String.class)
                 .setShowTotal(false);
 
+        CrosstabRowGroupBuilder<Integer> sortOrderGroup = ctab.rowGroup("sort_order", Integer.class)
+                .setShowTotal(false)
+                .setHeaderWidth(15)
+                .setOrderType(OrderType.ASCENDING);
+
         CrosstabColumnGroupBuilder<String> columnGroup = ctab.columnGroup("concept_name", String.class)
                 .setShowTotal(false);
 
         CrosstabBuilder crosstab = ctab.crosstab()
                 .headerCell(DynamicReports.cmp.text("Age Group / Outcome"))
-                .rowGroups(rowGroup)
+                .rowGroups(sortOrderGroup, rowGroup)
                 .columnGroups(columnGroup)
                 .measures(
                         ctab.measure("Female", "female_count", Integer.class, Calculation.NOTHING),
                         ctab.measure("Male", "male_count", Integer.class, Calculation.NOTHING),
+                        ctab.measure("Other", "other_count", Integer.class, Calculation.NOTHING),
                         ctab.measure("Total", "total_count", Integer.class, Calculation.NOTHING)
                 )
                 .setCellStyle(Templates.columnStyle.setBorder(Styles.pen()));
--- src/main/resources/sql/obsCountByGenderAndAgeGroupQuery.sql
+++ src/main/resources/sql/obsCountByGenderAndAgeGroupQuery.sql
@@ -1,8 +1,10 @@
   SELECT distinct answer.concept_full_name as concept_name,     
-                  reporting_age_group.name AS age_group,     
+                  reporting_age_group.name AS age_group,
+                  reporting_age_group.sort_order as sort_order,
                   IF(inpatient_outcome_obs.age_group_id IS NULL, 0, SUM(IF(inpatient_outcome_obs.gender = 'F', 1, 0))) AS female_count,     
                   IF(inpatient_outcome_obs.age_group_id IS NULL, 0, SUM(IF(inpatient_outcome_obs.gender = 'M', 1, 0))) AS male_count,     
-                  IF(inpatient_outcome_obs.age_group_id IS NULL, 0, COUNT(obs_id)) as total_count     
+                  IF(inpatient_outcome_obs.age_group_id IS NULL, 0, SUM(IF(inpatient_outcome_obs.gender = 'O', 1, 0))) AS other_count,
+                  IF(inpatient_outcome_obs.age_group_id IS NULL, 0, COUNT(obs_id)) as total_count
                   FROM concept_view as question     
                   JOIN concept_answer ON question.concept_id = concept_answer.concept_id     
                   JOIN concept_view as answer ON answer.concept_id = concept_answer.answer_concept     
@@ -12,7 +14,7 @@
                   FROM valid_coded_obs_view      
                   LEFT OUTER JOIN person ON valid_coded_obs_view.person_id = person.person_id     
                   LEFT OUTER JOIN reporting_age_group as observed_age_group ON observed_age_group.report_group_name = '%s' AND
-                  valid_coded_obs_view.obs_datetime BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))      
+                  cast(valid_coded_obs_view.obs_datetime as date) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
                   AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))      
                   WHERE cast(valid_coded_obs_view.obs_datetime as date) BETWEEN '%s' AND '%s' AND valid_coded_obs_view.concept_full_name = '%s'
                   ) AS inpatient_outcome_obs ON inpatient_outcome_obs.concept_id = question.concept_id AND inpatient_outcome_obs.value_coded = answer.concept_id      
