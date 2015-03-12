47b0e3e731e3a925af84a8ebf81a5feabb8b0222
Vinay Venu <vinayv@thoughtworks.com>
2015-03-12 11:26:08 +0530

    Add target and retention values so the annotation is actually usable

--- src/main/java/org/bahmni/reports/model/UsingDatasource.java
+++ src/main/java/org/bahmni/reports/model/UsingDatasource.java
@@ -1,5 +1,12 @@
 package org.bahmni.reports.model;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface UsingDatasource {
     String value();
 }
