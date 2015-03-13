e8edaa446da78df9015b829af389e76c0c050d2d
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-03-13 15:42:09 +0530

    Banka | Using String Template to replace placeholders in sqls with proper value.

--- src/main/java/org/bahmni/reports/template/SqlReportTemplate.java
+++ src/main/java/org/bahmni/reports/template/SqlReportTemplate.java
@@ -6,8 +6,13 @@ import net.sf.dynamicreports.report.constant.PageType;
 import net.sf.dynamicreports.report.exception.DRException;
 import org.bahmni.reports.model.Report;
 import org.bahmni.reports.model.SqlReportConfig;
+import org.stringtemplate.v4.ST;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import static net.sf.dynamicreports.report.builder.DynamicReports.col;
@@ -19,16 +24,15 @@ public class SqlReportTemplate  implements BaseReportTemplate<SqlReportConfig> {
 
     @Override
     public JasperReportBuilder build(Connection connection, Report<SqlReportConfig> reportConfig, String startDate, String endDate, List<AutoCloseable> resources) throws SQLException, DRException {
-        String sql = getFileContent(reportConfig.getConfig().getSqlPath(), true);
         JasperReportBuilder report = report();
         report.setPageFormat(PageType.A3, PageOrientation.LANDSCAPE)
                 .setTemplate(Templates.reportTemplate)
                 .setReportName(reportConfig.getName())
                 .pageFooter(Templates.footerComponent);
 
-
+        String sqlString = getSqlString(reportConfig, startDate, endDate);
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(String.format(sql, startDate, endDate));
+        ResultSet resultSet = statement.executeQuery(sqlString);
         ResultSetMetaData metaData = resultSet.getMetaData();
         int columnCount = metaData.getColumnCount();
         for(int i = 1; i <= columnCount; i++) {
@@ -38,6 +42,14 @@ public class SqlReportTemplate  implements BaseReportTemplate<SqlReportConfig> {
         report.setDataSource(resultSet);
         resources.add(statement);
         return report;
+    }
+
+    private String getSqlString(Report<SqlReportConfig> reportConfig, String startDate, String endDate) {
+        String sql = getFileContent(reportConfig.getConfig().getSqlPath(), true);
+        ST sqlTemplate = new ST(sql, '#', '#');
+        sqlTemplate.add("startDate", startDate);
+        sqlTemplate.add("endDate", endDate);
+        return sqlTemplate.render();
     }
 
 }
