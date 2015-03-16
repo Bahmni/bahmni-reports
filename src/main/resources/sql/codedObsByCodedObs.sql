18b83c540aadcf1e9850aff1f78c313e3b9e6200
Vinay Venu <vinayv@thoughtworks.com>
2015-03-16 11:52:05 +0530

    Chethan, Vinay | Adding visit based filter for codedObs Vs codedObs query

--- src/main/resources/sql/codedObsByCodedObs.sql
+++ src/main/resources/sql/codedObsByCodedObs.sql
@@ -16,7 +16,7 @@ FROM
      INNER JOIN concept_name cn2 ON ca1.answer_concept = cn2.concept_id AND
                                     cn2.voided IS FALSE AND cn2.concept_name_type = 'FULLY_SPECIFIED'
      LEFT JOIN concept_name cn3 ON ca1.answer_concept = cn3.concept_id AND
-                                    cn3.voided IS FALSE AND cn3.concept_name_type = 'SHORT'
+                                   cn3.voided IS FALSE AND cn3.concept_name_type = 'SHORT'
   ) first_answers
   INNER JOIN
   (SELECT
@@ -30,16 +30,15 @@ FROM
      LEFT JOIN concept_name cn3 ON ca1.answer_concept = cn3.concept_id AND
                                    cn3.voided IS FALSE AND cn3.concept_name_type = 'SHORT'
   ) second_answers
-  INNER JOIN (SELECT DISTINCT gender
-              FROM person
-              WHERE gender IN ('M', 'F')) gender
+  INNER JOIN (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
   INNER JOIN reporting_age_group rag ON rag.report_group_name = '%s'
   LEFT OUTER JOIN (
-                    SELECT
+                    SELECT DISTINCT
                       o1.person_id,
                       cn2.concept_id AS    answer,
                       cn1.concept_id AS    question,
-                      max(o1.obs_datetime) obsdatetime
+                      e1.visit_id,
+                      max(e1.encounter_datetime) AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
@@ -49,12 +48,14 @@ FROM
                         ON o1.value_coded = cn2.concept_id
                            AND cn2.concept_name_type = 'FULLY_SPECIFIED'
                            AND cn2.voided = 0
+                      INNER JOIN encounter e1
+                        ON o1.encounter_id = e1.encounter_id
                     WHERE o1.obs_datetime BETWEEN '%s' AND '%s'
-                    GROUP BY o1.person_id, cn2.name
+                    GROUP BY o1.person_id, cn2.concept_id, cn1.concept_id, e1.visit_id
                   ) first_concept
     ON first_concept.answer = first_answers.answer
   LEFT OUTER JOIN (
-                    SELECT
+                    SELECT DISTINCT
                       o1.person_id,
                       cn2.concept_id AS answer,
                       cn1.concept_id AS question
@@ -68,12 +69,14 @@ FROM
                         ON o1.value_coded = cn2.concept_id
                            AND cn2.concept_name_type = 'FULLY_SPECIFIED'
                            AND cn2.voided = 0
+                      INNER JOIN encounter e1
+                        ON o1.encounter_id = e1.encounter_id
                     WHERE o1.obs_datetime BETWEEN '%s' AND '%s'
                   ) second_concept
     ON second_concept.answer = second_answers.answer
        AND first_concept.person_id = second_concept.person_id
   LEFT OUTER JOIN person p ON first_concept.person_id = p.person_id AND p.gender = gender.gender
-                              AND cast(first_concept.obsdatetime AS DATE) BETWEEN (DATE_ADD(
+                              AND cast(first_concept.datetime AS DATE) BETWEEN (DATE_ADD(
     DATE_ADD(p.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days
     DAY)) AND (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.max_years YEAR), INTERVAL
                         rag.max_days DAY))
