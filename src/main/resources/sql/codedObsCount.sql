1b1595548ab24213556e23beaff3a1e51c39ee01
Vinay Venu <vinayv@thoughtworks.com>
2015-03-16 15:52:45 +0530

    Remove hardcoding of visit attribute value references

--- src/main/resources/sql/codedObsCount.sql
+++ src/main/resources/sql/codedObsCount.sql
@@ -4,23 +4,17 @@ SELECT
   IF(visit_type.type IN ('Admitted', 'Discharged'), 'IPD', visit_type.type)                              AS visit,
   IF(answer.concept_short_name IS NULL, answer.concept_full_name, answer.concept_short_name)             AS answer_concept_name,
   reporting_age_group.sort_order                                                                         AS sort_order,
-  IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'F', 1, 0) AS female_count,
-  IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'M', 1, 0) AS male_count,
-  IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'O', 1, 0) AS other_count,
-  IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL, 1, 0)                         AS total_count
+  sum(IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'F', 1, 0)) AS female_count,
+  sum(IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'M', 1, 0)) AS male_count,
+  sum(IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL AND person.gender = 'O', 1, 0)) AS other_count,
+  sum(IF(person.person_id IS NOT NULL AND attr.visit_attribute_id IS NOT NULL, 1, 0))                         AS total_count
 FROM
   concept_view AS question
   INNER JOIN concept_answer
     ON question.concept_id = concept_answer.concept_id AND question.concept_full_name IN (%s)
   INNER JOIN concept_view AS answer
     ON answer.concept_id = concept_answer.answer_concept
-  INNER JOIN (SELECT 'Admitted' AS 'type'
-              UNION SELECT 'Discharged' AS 'type'
-              UNION SELECT 'OPD' AS 'type'
-              UNION SELECT 'Special OPD' AS 'type'
-              UNION SELECT 'EMERGENCY' AS 'type'
-              UNION SELECT 'PHARMACY VISIT' AS 'type'
-              UNION SELECT 'LAB VISIT' AS 'type') visit_type %s
+  INNER JOIN (SELECT distinct value_reference as type from visit_attribute) visit_type %s
   INNER JOIN reporting_age_group
     ON reporting_age_group.report_group_name = '%s'
   LEFT JOIN obs ON obs.concept_id = question.concept_id AND obs.value_coded = answer.concept_id AND cast(obs.obs_datetime AS DATE) BETWEEN '%s' AND '%s'
@@ -33,3 +27,4 @@ FROM
   LEFT JOIN visit visit ON enc.visit_id = visit.visit_id
   LEFT JOIN visit_attribute AS attr ON visit.visit_id = attr.visit_id AND attr.value_reference = visit_type.type
   LEFT JOIN visit_attribute_type AS attr_type ON attr_type.visit_attribute_type_id = attr.attribute_type_id AND attr_type.name = 'VisitStatus'
+  group by age_group, concept_name, visit, answer_concept_name, sort_order
