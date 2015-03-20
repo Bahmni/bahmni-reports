6f7997dee438bf08a2f610d42c385dda94e3ba9a
Preethi <preethis@thoughtworks.com>
2015-03-20 15:42:19 +0530

    Preethi, Sudhakar | #CannedReports | Ignoring revised diagnoses in diagnosis count reports

--- src/main/resources/sql/diagnosisCountByAgeGroup.sql
+++ src/main/resources/sql/diagnosisCountByAgeGroup.sql
@@ -33,16 +33,27 @@ FROM diagnosis_concept_view
                                      confirmed.concept_name_type = 'FULLY_SPECIFIED'
                             WHERE parent.voided IS FALSE ) AS confirmed
                        WHERE confirmed.obs_id NOT IN (SELECT parent.obs_id
-                                                      FROM obs AS parent
-                                                        JOIN concept_view pcv2
-                                                          ON pcv2.concept_id = parent.concept_id AND
-                                                             pcv2.concept_full_name = 'Visit Diagnoses'
-                                                        LEFT JOIN obs AS STATUS ON STATUS.obs_group_id = parent.obs_id
-                                                        JOIN concept_name
-                                                          ON concept_name.concept_id = STATUS.value_coded AND
-                                                             concept_name.name = 'Ruled Out Diagnosis' AND
-                                                             concept_name.concept_name_type =
-                                                             'FULLY_SPECIFIED'))) AS filtered_diagnosis
+                                      FROM obs AS parent
+                                      JOIN concept_view pcv2 ON pcv2.concept_id = parent.concept_id AND pcv2.concept_full_name = 'Visit Diagnoses'
+                                      JOIN (
+                                            SELECT obs_group_id
+                                            FROM obs AS status
+                                              JOIN concept_name ON concept_name.concept_id = status.value_coded AND
+                                                   concept_name.name = 'Ruled Out Diagnosis' AND
+                                                   concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                   status.voided IS FALSE
+                                            UNION
+                                            SELECT obs_group_id
+                                            FROM obs AS revised
+                                              JOIN concept_name revised_concept
+                                                ON revised_concept.concept_id = revised.concept_id AND
+                                                   revised_concept.name = 'Bahmni Diagnosis Revised' AND
+                                                   revised_concept.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                   revised.value_coded = (SELECT property_value FROM global_property WHERE property = 'concept.true') AND
+                                                   revised.voided IS FALSE
+                                      ) revised_and_ruled_out_diagnosis
+                                      ON revised_and_ruled_out_diagnosis.obs_group_id = parent.obs_id
+                                      WHERE parent.voided IS FALSE))) AS filtered_diagnosis
 
   JOIN person
     ON person.person_id = filtered_diagnosis.person_id
