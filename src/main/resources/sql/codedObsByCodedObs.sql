80f85c6fd914bd3ddb9efd3a07ce06257b8869a6
Vinay Venu <vinayv@thoughtworks.com>
2015-03-20 14:27:21 +0530

    Count latest obs once per visit.

--- src/main/resources/sql/codedObsByCodedObs.sql
+++ src/main/resources/sql/codedObsByCodedObs.sql
@@ -1,11 +1,11 @@
 SELECT
-  first_answers.answer_name as first_concept_name,
-  second_answers.answer_name as second_concept_name,
-  gender.gender as gender,
-  rag.name as age_group,
-  rag.sort_order as age_group_sort_order,
+  first_answers.answer_name  AS first_concept_name,
+  second_answers.answer_name AS second_concept_name,
+  gender.gender              AS gender,
+  rag.name                   AS age_group,
+  rag.sort_order             AS age_group_sort_order,
   sum(CASE WHEN first_concept.answer IS NOT NULL AND second_concept.answer IS NOT NULL AND p.gender IS NOT NULL THEN 1
-      ELSE 0 END) as patient_count
+      ELSE 0 END)            AS patient_count
 FROM
   (SELECT
      ca.answer_concept                                                                AS answer,
@@ -13,7 +13,8 @@ FROM
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
      INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
      INNER JOIN concept_name answer_concept_fully_specified_name
@@ -26,14 +27,15 @@ FROM
    WHERE question_concept_name.name = '#firstConcept#' AND cd.name = 'Coded'
    UNION
    SELECT
-     answer_concept_fully_specified_name.concept_id                                                                AS answer,
-     answer_concept_fully_specified_name.name AS answer_name
+     answer_concept_fully_specified_name.concept_id AS answer,
+     answer_concept_fully_specified_name.name       AS answer_name
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
-     INNER JOIN global_property gp ON gp.property in ('concept.true', 'concept.false')
+     INNER JOIN global_property gp ON gp.property IN ('concept.true', 'concept.false')
      INNER JOIN concept_name answer_concept_fully_specified_name
        ON answer_concept_fully_specified_name.concept_id = gp.property_value
           AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED' AND
@@ -47,7 +49,8 @@ FROM
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
      INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
      INNER JOIN concept_name answer_concept_fully_specified_name
@@ -60,14 +63,15 @@ FROM
    WHERE question_concept_name.name = '#secondConcept#' AND cd.name = 'Coded'
    UNION
    SELECT
-     answer_concept_fully_specified_name.concept_id                                                                AS answer,
-     answer_concept_fully_specified_name.name AS answer_name
+     answer_concept_fully_specified_name.concept_id AS answer,
+     answer_concept_fully_specified_name.name       AS answer_name
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
-     INNER JOIN global_property gp ON gp.property in ('concept.true', 'concept.false')
+     INNER JOIN global_property gp ON gp.property IN ('concept.true', 'concept.false')
      INNER JOIN concept_name answer_concept_fully_specified_name
        ON answer_concept_fully_specified_name.concept_id = gp.property_value
           AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED' AND
@@ -75,15 +79,17 @@ FROM
    WHERE question_concept_name.name = '#secondConcept#' AND cd.name = 'Boolean'
    ORDER BY answer_name DESC
   ) second_answers
-  INNER JOIN (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
+  INNER JOIN (SELECT 'M' AS gender
+              UNION SELECT 'F' AS gender
+              UNION SELECT 'O' AS gender) gender
   INNER JOIN reporting_age_group rag ON rag.report_group_name = '#reportGroupName#'
   LEFT OUTER JOIN (
-                    SELECT DISTINCT
+                    SELECT
                       o1.person_id,
-                      cn2.concept_id AS    answer,
-                      cn1.concept_id AS    question,
-                      v1.visit_id,
-                      max(v1.date_stopped) AS datetime
+                      cn2.concept_id  AS answer,
+                      cn1.concept_id  AS question,
+                      v1.visit_id     AS visit_id,
+                      v1.date_stopped AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
@@ -97,21 +103,27 @@ FROM
                         ON o1.encounter_id = e1.encounter_id
                       INNER JOIN visit v1
                         ON v1.visit_id = e1.visit_id
-                          AND v1.date_stopped IS NOT NULL
-                    WHERE cast(v1.date_stopped as date) BETWEEN '#startDate#' AND '#endDate#'
-                    GROUP BY o1.person_id, cn2.concept_id, cn1.concept_id, e1.visit_id
+                           AND v1.date_stopped IS NOT NULL
+                    WHERE cast(v1.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+                          AND o1.obs_id = (SELECT max(obs_id)
+                                           FROM obs obs2
+                                             INNER JOIN encounter e2 ON obs2.encounter_id = e2.encounter_id
+                                           WHERE obs2.concept_id = o1.concept_id
+                                                 AND e2.visit_id = v1.visit_id
+                                                 AND obs2.voided IS FALSE)
                   ) first_concept
     ON first_concept.answer = first_answers.answer
   LEFT OUTER JOIN (
-                    SELECT DISTINCT
+                    SELECT
                       o1.person_id,
-                      cn2.concept_id AS answer,
-                      cn1.concept_id AS question
+                      cn2.concept_id  AS answer,
+                      cn1.concept_id  AS question,
+                      v1.visit_id     AS visit_id,
+                      v1.date_stopped AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
-                           cn1.concept_name_type = 'FULLY_SPECIFIED'
-                           AND cn1.name = '#secondConcept#'
+                           cn1.concept_name_type = 'FULLY_SPECIFIED' AND cn1.name = '#secondConcept#'
                            AND o1.voided = 0 AND cn1.voided = 0
                       INNER JOIN concept_name cn2
                         ON o1.value_coded = cn2.concept_id
@@ -121,10 +133,18 @@ FROM
                         ON o1.encounter_id = e1.encounter_id
                       INNER JOIN visit v1
                         ON v1.visit_id = e1.visit_id
-                    WHERE cast(v1.date_stopped as date) BETWEEN '#startDate#' AND '#endDate#'
+                           AND v1.date_stopped IS NOT NULL
+                    WHERE cast(v1.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+                          AND o1.obs_id = (SELECT max(obs_id)
+                                           FROM obs obs2
+                                             INNER JOIN encounter e2 ON obs2.encounter_id = e2.encounter_id
+                                           WHERE obs2.concept_id = o1.concept_id
+                                                 AND e2.visit_id = v1.visit_id
+                                                 AND obs2.voided IS FALSE)
                   ) second_concept
     ON second_concept.answer = second_answers.answer
        AND first_concept.person_id = second_concept.person_id
+       AND first_concept.visit_id = second_concept.visit_id
   LEFT OUTER JOIN person p ON first_concept.person_id = p.person_id AND p.gender = gender.gender
                               AND cast(first_concept.datetime AS DATE) BETWEEN (DATE_ADD(
     DATE_ADD(p.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days
--- src/main/resources/sql/multipleCodedObsByCodedObs.sql
+++ src/main/resources/sql/multipleCodedObsByCodedObs.sql
@@ -1,12 +1,14 @@
 SELECT
-  first_answers.answer_name as concept1_name,
-  second_answers.answer_name as concept2_name,
-  third_answers.answer_name as concept3_name,
-  gender.gender as gender,
-  rag.name as age_group,
-  rag.sort_order as age_group_sort_order,
-  sum(CASE WHEN first_concept.answer IS NOT NULL AND second_concept.answer IS NOT NULL AND third_concept.answer IS NOT NULL AND p.gender IS NOT NULL THEN 1
-      ELSE 0 END) as patient_count
+  first_answers.answer_name  AS concept1_name,
+  second_answers.answer_name AS concept2_name,
+  third_answers.answer_name  AS concept3_name,
+  gender.gender              AS gender,
+  rag.name                   AS age_group,
+  rag.sort_order             AS age_group_sort_order,
+  sum(CASE WHEN
+    first_concept.answer IS NOT NULL AND second_concept.answer IS NOT NULL AND third_concept.answer IS NOT NULL AND
+    p.gender IS NOT NULL THEN 1
+      ELSE 0 END)            AS patient_count
 FROM
   (SELECT
      ca.answer_concept                                                                AS answer,
@@ -14,7 +16,8 @@ FROM
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
      INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
      INNER JOIN concept_name answer_concept_fully_specified_name
@@ -27,14 +30,15 @@ FROM
    WHERE question_concept_name.name = '#concept1#' AND cd.name = 'Coded'
    UNION
    SELECT
-     answer_concept_fully_specified_name.concept_id                                                                AS answer,
-     answer_concept_fully_specified_name.name AS answer_name
+     answer_concept_fully_specified_name.concept_id AS answer,
+     answer_concept_fully_specified_name.name       AS answer_name
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
-     INNER JOIN global_property gp ON gp.property in ('concept.true', 'concept.false')
+     INNER JOIN global_property gp ON gp.property IN ('concept.true', 'concept.false')
      INNER JOIN concept_name answer_concept_fully_specified_name
        ON answer_concept_fully_specified_name.concept_id = gp.property_value
           AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED' AND
@@ -48,7 +52,8 @@ FROM
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
      INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
      INNER JOIN concept_name answer_concept_fully_specified_name
@@ -61,14 +66,15 @@ FROM
    WHERE question_concept_name.name = '#concept2#' AND cd.name = 'Coded'
    UNION
    SELECT
-     answer_concept_fully_specified_name.concept_id                                                                AS answer,
-     answer_concept_fully_specified_name.name AS answer_name
+     answer_concept_fully_specified_name.concept_id AS answer,
+     answer_concept_fully_specified_name.name       AS answer_name
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
-     INNER JOIN global_property gp ON gp.property in ('concept.true', 'concept.false')
+     INNER JOIN global_property gp ON gp.property IN ('concept.true', 'concept.false')
      INNER JOIN concept_name answer_concept_fully_specified_name
        ON answer_concept_fully_specified_name.concept_id = gp.property_value
           AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED' AND
@@ -84,7 +90,8 @@ FROM
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
      INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
      INNER JOIN concept_name answer_concept_fully_specified_name
@@ -97,14 +104,15 @@ FROM
    WHERE question_concept_name.name = '#concept3#' AND cd.name = 'Coded'
    UNION
    SELECT
-     answer_concept_fully_specified_name.concept_id                                                                AS answer,
-     answer_concept_fully_specified_name.name AS answer_name
+     answer_concept_fully_specified_name.concept_id AS answer,
+     answer_concept_fully_specified_name.name       AS answer_name
    FROM concept c
      INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
      INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED' AND
+                                                      AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+                                                      AND
                                                       question_concept_name.voided IS FALSE
-     INNER JOIN global_property gp ON gp.property in ('concept.true', 'concept.false')
+     INNER JOIN global_property gp ON gp.property IN ('concept.true', 'concept.false')
      INNER JOIN concept_name answer_concept_fully_specified_name
        ON answer_concept_fully_specified_name.concept_id = gp.property_value
           AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED' AND
@@ -112,20 +120,21 @@ FROM
    WHERE question_concept_name.name = '#concept3#' AND cd.name = 'Boolean'
    ORDER BY answer_name DESC
   ) third_answers
-
-  INNER JOIN (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
+  INNER JOIN (SELECT 'M' AS gender
+              UNION SELECT 'F' AS gender
+              UNION SELECT 'O' AS gender) gender
   INNER JOIN reporting_age_group rag ON rag.report_group_name = '#reportGroupName#'
   LEFT OUTER JOIN (
-                    SELECT DISTINCT
+                    SELECT
                       o1.person_id,
-                      cn2.concept_id AS    answer,
-                      cn1.concept_id AS    question,
-                      e1.visit_id,
-                      max(v1.date_stopped) AS datetime
+                      cn2.concept_id  AS answer,
+                      cn1.concept_id  AS question,
+                      v1.visit_id     AS visit_id,
+                      v1.date_stopped AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
-                           cn1.concept_name_type = 'FULLY_SPECIFIED' AND cn1.name = '#concept1#'
+                           cn1.concept_name_type = 'FULLY_SPECIFIED' AND cn1.name = '#firstConcept#'
                            AND o1.voided = 0 AND cn1.voided = 0
                       INNER JOIN concept_name cn2
                         ON o1.value_coded = cn2.concept_id
@@ -135,20 +144,27 @@ FROM
                         ON o1.encounter_id = e1.encounter_id
                       INNER JOIN visit v1
                         ON v1.visit_id = e1.visit_id
-                    WHERE cast(v1.date_stopped as date) BETWEEN '#startDate#' AND '#endDate#'
-                    GROUP BY o1.person_id, cn2.concept_id, cn1.concept_id, e1.visit_id
+                           AND v1.date_stopped IS NOT NULL
+                    WHERE cast(v1.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+                          AND o1.obs_id = (SELECT max(obs_id)
+                                           FROM obs obs2
+                                             INNER JOIN encounter e2 ON obs2.encounter_id = e2.encounter_id
+                                           WHERE obs2.concept_id = o1.concept_id
+                                                 AND e2.visit_id = v1.visit_id
+                                                 AND obs2.voided IS FALSE)
                   ) first_concept
     ON first_concept.answer = first_answers.answer
   LEFT OUTER JOIN (
-                    SELECT DISTINCT
+                    SELECT
                       o1.person_id,
-                      cn2.concept_id AS answer,
-                      cn1.concept_id AS question
+                      cn2.concept_id  AS answer,
+                      cn1.concept_id  AS question,
+                      v1.visit_id     AS visit_id,
+                      v1.date_stopped AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
-                           cn1.concept_name_type = 'FULLY_SPECIFIED'
-                           AND cn1.name = '#concept2#'
+                           cn1.concept_name_type = 'FULLY_SPECIFIED' AND cn1.name = '#firstConcept#'
                            AND o1.voided = 0 AND cn1.voided = 0
                       INNER JOIN concept_name cn2
                         ON o1.value_coded = cn2.concept_id
@@ -158,19 +174,29 @@ FROM
                         ON o1.encounter_id = e1.encounter_id
                       INNER JOIN visit v1
                         ON v1.visit_id = e1.visit_id
-                    WHERE cast(v1.date_stopped as date) BETWEEN '#startDate#' AND '#endDate#'
+                           AND v1.date_stopped IS NOT NULL
+                    WHERE cast(v1.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+                          AND o1.obs_id = (SELECT max(obs_id)
+                                           FROM obs obs2
+                                             INNER JOIN encounter e2 ON obs2.encounter_id = e2.encounter_id
+                                           WHERE obs2.concept_id = o1.concept_id
+                                                 AND e2.visit_id = v1.visit_id
+                                                 AND obs2.voided IS FALSE)
                   ) second_concept
     ON second_concept.answer = second_answers.answer
+       AND first_concept.visit_id = second_concept.visit_id
+       AND first_concept.person_id = second_concept.person_id
   LEFT OUTER JOIN (
-                    SELECT DISTINCT
+                    SELECT
                       o1.person_id,
-                      cn2.concept_id AS answer,
-                      cn1.concept_id AS question
+                      cn2.concept_id  AS answer,
+                      cn1.concept_id  AS question,
+                      v1.visit_id     AS visit_id,
+                      v1.date_stopped AS datetime
                     FROM obs o1
                       INNER JOIN concept_name cn1
                         ON o1.concept_id = cn1.concept_id AND
-                           cn1.concept_name_type = 'FULLY_SPECIFIED'
-                           AND cn1.name = '#concept3#'
+                           cn1.concept_name_type = 'FULLY_SPECIFIED' AND cn1.name = '#firstConcept#'
                            AND o1.voided = 0 AND cn1.voided = 0
                       INNER JOIN concept_name cn2
                         ON o1.value_coded = cn2.concept_id
@@ -180,14 +206,22 @@ FROM
                         ON o1.encounter_id = e1.encounter_id
                       INNER JOIN visit v1
                         ON v1.visit_id = e1.visit_id
-                    WHERE cast(v1.date_stopped as date) BETWEEN '#startDate#' AND '#endDate#'
+                           AND v1.date_stopped IS NOT NULL
+                    WHERE cast(v1.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+                          AND o1.obs_id = (SELECT max(obs_id)
+                                           FROM obs obs2
+                                             INNER JOIN encounter e2 ON obs2.encounter_id = e2.encounter_id
+                                           WHERE obs2.concept_id = o1.concept_id
+                                                 AND e2.visit_id = v1.visit_id
+                                                 AND obs2.voided IS FALSE)
                   ) third_concept
     ON third_concept.answer = third_answers.answer
-       AND first_concept.person_id = second_concept.person_id
        AND first_concept.person_id = third_concept.person_id
+       AND first_concept.visit_id = third_concept.visit_id
   LEFT OUTER JOIN person p ON first_concept.person_id = p.person_id AND p.gender = gender.gender
                               AND cast(first_concept.datetime AS DATE) BETWEEN (DATE_ADD(
     DATE_ADD(p.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days
     DAY)) AND (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.max_years YEAR), INTERVAL
                         rag.max_days DAY))
-GROUP BY first_answers.answer_name, second_answers.answer_name, third_answers.answer_name, gender.gender, rag.name, rag.sort_order;
+GROUP BY first_answers.answer_name, second_answers.answer_name, third_answers.answer_name, gender.gender, rag.name,
+  rag.sort_order;
