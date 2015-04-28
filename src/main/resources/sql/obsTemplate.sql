d95813e0f27b77a25eae2e2e944c16e2add1a216
Preethi <preethis@thoughtworks.com>
2015-04-28 13:55:43 +0530

    Preethi, Gautam | #1932 | Fixed a bug in which obs are not filtered based on date filter

--- src/main/resources/sql/obsTemplate.sql
+++ src/main/resources/sql/obsTemplate.sql
@@ -5,35 +5,53 @@ SELECT
                CONCAT(
                    'GROUP_CONCAT(IF(cv.concept_full_name = ''',
                    concept_full_name,
-                   ''', coalesce(ob.value_numeric, ob.value_boolean, ob.value_datetime, ob.value_text, answer.concept_short_name, answer.concept_full_name), NULL) SEPARATOR \',\') AS `',
+                   ''', coalesce(o.value_numeric, o.value_boolean, o.value_datetime, o.value_text, o.concept_short_name, o.concept_full_name), NULL) SEPARATOR \',\') AS `',
                    concept_full_name , '`'
                )
   ) into @sql
 FROM concept_view cv where cv.concept_full_name in (%s);
 
 SET @sql = CONCAT('SELECT
-                  pi.identifier,
-                  concat(pat_name.given_name, \' \', pat_name.family_name) AS patient_name,
-                  person.gender,
-                  floor(DATEDIFF(DATE(ob.obs_datetime), person.birthdate) / 365)      AS age,
-                  ep.provider_id,
-                  ep.encounter_id,
-                  concat(pn.given_name, \' \', pn.family_name) AS provider_name,
-                  e.encounter_datetime,',
+                  o.identifier,
+                  o.patient_name,
+                  o.age,
+                  o.gender,
+                  o.provider_id,
+                  o.encounter_id,
+                  o.provider_name,
+                  o.encounter_datetime,',
                  @sql,
                   ' FROM concept_view cv
-                  LEFT JOIN obs ob ON ob.concept_id = cv.concept_id AND ob.voided is false
-                  LEFT JOIN concept_view answer on ob.value_coded = answer.concept_id
-                  LEFT JOIN encounter e ON ob.encounter_id = e.encounter_id and cast(e.encounter_datetime AS DATE) BETWEEN \'%s\' AND \'%s\'
-                  LEFT JOIN patient_identifier pi ON pi.patient_id = ob.person_id
-                  LEFT JOIN person on person.person_id = pi.patient_id
-                  LEFT JOIN person_name pat_name on pat_name.person_id = person.person_id
-                  LEFT JOIN encounter_provider ep ON e.encounter_id = ep.encounter_id
-                  LEFT JOIN provider p ON ep.provider_id = p.provider_id
-                  LEFT JOIN person_name pn on p.person_id = pn.person_id
+                    LEFT JOIN
+                    (SELECT
+                       pi.identifier,
+                       ob.concept_id,
+                       concat(pat_name.given_name, '' '', pat_name.family_name) AS patient_name,
+                       floor(DATEDIFF(DATE(ob.obs_datetime), person.birthdate) / 365)   AS age,
+                       person.gender,
+                       ep.provider_id,
+                       ep.encounter_id,
+                       concat(pn.given_name, '' '', pn.family_name)   AS provider_name,
+                       e.encounter_datetime,
+                       ob.value_numeric,
+                       ob.value_boolean,
+                       ob.value_datetime,
+                       ob.value_text,
+                       answer.concept_short_name,
+                       answer.concept_full_name
+                     FROM obs ob
+                       JOIN encounter e ON ob.encounter_id = e.encounter_id AND cast(e.encounter_datetime AS DATE) BETWEEN \'%s\' AND \'%s\'  AND ob.voided IS FALSE
+                       JOIN patient_identifier pi ON pi.patient_id = ob.person_id
+                       JOIN person ON person.person_id = pi.patient_id
+                       JOIN person_name pat_name ON pat_name.person_id = person.person_id
+                       JOIN encounter_provider ep ON e.encounter_id = ep.encounter_id
+                       JOIN provider p ON ep.provider_id = p.provider_id
+                       JOIN person_name pn ON p.person_id = pn.person_id
+                       LEFT JOIN concept_view answer ON ob.value_coded = answer.concept_id
+                       ) o ON o.concept_id = cv.concept_id
                   where cv.concept_full_name IN ( %s )
                   group by identifier, encounter_id, provider_id
-                  order by identifier, e.encounter_id');
+                  order by identifier, encounter_id');
 
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
