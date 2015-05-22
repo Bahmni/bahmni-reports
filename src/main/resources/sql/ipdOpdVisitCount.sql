b6ab95677fc129541a2db57483ccebcf94da1881
Mahesh Kumar <maheshkv@thoughtworks.com>
2015-05-22 08:24:49 +0530

    Mahesh | Minor change in IPD/OPD visit counts report

--- src/main/resources/sql/ipdOpdVisitCount.sql
+++ src/main/resources/sql/ipdOpdVisitCount.sql
@@ -5,7 +5,7 @@ select 	ifnull(sum(if(value_reference='OPD' and date_started=date_created,1,0)),
         ifnull(sum(if(value_reference='IPD' and date_started!=date_created,1,0)),0) as Old_IPD,
         ifnull(sum(if(value_reference='IPD',1,0)),0) as Total_IPD
         from
-		(select v.visit_id, va.value_reference, p.person_id, v.date_started, p.date_created from visit v
+		(select v.visit_id, va.value_reference, p.person_id, date(v.date_started) as date_started, date(p.date_created) as date_created from visit v
 			inner join person p on p.person_id=v.patient_id
 			inner join visit_attribute va on va.visit_id=v.visit_id
-            where v.date_started between '%s' and '%s') as raw_result;
+            where date(v.date_started) between '%s' and '%s') as raw_result;
