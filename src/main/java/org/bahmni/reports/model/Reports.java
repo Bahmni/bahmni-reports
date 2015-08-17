1bbc0c69a450aa0cec59f08f86600e17beab357e
sravanthi17 <sravanns@thoughtworks.com>
2015-08-17 14:39:10 +0530

    Shireesha, Sravanthi | # 2679 | Reading config as hash instead of array

--- src/main/java/org/bahmni/reports/model/Reports.java
+++ src/main/java/org/bahmni/reports/model/Reports.java
@@ -6,15 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Reports extends ArrayList<Report> {
+public class Reports extends HashMap<String, Report> {
 
     public static Report find(String reportName, String reportPropertiesPath) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Reports reports = objectMapper.readValue(new File(reportPropertiesPath), Reports.class);
-        for (Report report : reports) {
+        for (Report report : reports.values()) {
             if (reportName.equals(report.getName())) {
                 return report;
             }
