956823e7dc0e1dff7af02fe326477c7cbe00c94d
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-10-16 09:38:53 +0530

    Sravanthi, Jaswanth | Added maven exec plugin to initialize database during test compile.

--- src/test/resources/create_db.sh
+++ src/test/resources/create_db.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+HOST=localhost
+TARGET_DB=reports_integration_tests
+DUMP_FILE=src/test/resources/openmrs_schema.sql
+USER_NAME=openmrs-user
+PASSWORD=password
+
+
+echo "Dropping database for integration tests ..."
+mysql -u$USER_NAME -p$PASSWORD -h$HOST -e "DROP DATABASE $TARGET_DB"
+
 echo "Creating database for integration tests ..."
-mysql -uroot -ppassword -hlocalhost reports_integration_tests < src/test/resources/openmrs_schema.sql
+mysql -u$USER_NAME -p$PASSWORD -h$HOST -e "CREATE DATABASE $TARGET_DB"
+
+echo "Applying database dump ..."
+mysql -u$USER_NAME -p$PASSWORD -h$HOST $TARGET_DB < $DUMP_FILE
