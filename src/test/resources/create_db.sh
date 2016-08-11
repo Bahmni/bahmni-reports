97e8ff39bdf88d6604e2bcff8d9a8ff90e2ef2a9
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-08-11 13:27:09 +0530

    Jaswanth | #0000 | Pick up port of mysql server from properties file

--- src/test/resources/create_db.sh
+++ src/test/resources/create_db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 HOST=$(grep openmrs.url $HOME/.bahmni-reports/bahmni-reports-test.properties | cut -d '/' -f 3 | cut -d ':' -f 1)
+PORT=$(grep openmrs.url $HOME/.bahmni-reports/bahmni-reports-test.properties | cut -d ':' -f 4 | cut -d '/' -f 1)
 TARGET_DB=$(grep openmrs.url $HOME/.bahmni-reports/bahmni-reports-test.properties | cut -d '/' -f 4 | cut -d '?' -f 1)
 DUMP_FILE=src/test/resources/sql/openmrs_schema.sql
 USER_NAME=$(grep openmrs.username $HOME/.bahmni-reports/bahmni-reports-test.properties | cut -d '=' -f 2)
@@ -8,10 +9,10 @@ PASSWORD=$(grep openmrs.password $HOME/.bahmni-reports/bahmni-reports-test.prope
 
 
 echo "Dropping database for integration tests ..."
-mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST -e "DROP DATABASE $TARGET_DB"
+mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST -P$PORT -e "DROP DATABASE $TARGET_DB"
 
 echo "Creating database for integration tests ..."
-mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST -e "CREATE DATABASE $TARGET_DB"
+mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST -P$PORT -e "CREATE DATABASE $TARGET_DB"
 
 echo "Applying database dump ..."
-mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST $TARGET_DB < $DUMP_FILE
+mysql --protocol tcp -u$USER_NAME -p$PASSWORD -h$HOST -P$PORT $TARGET_DB < $DUMP_FILE
