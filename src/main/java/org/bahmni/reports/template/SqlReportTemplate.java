3a8b8065285ac3bba924ea2647b46cf2a0cc3c29
sumanmaity112 <suman.maity112@gmail.com>
2017-08-02 14:07:45 +0530

    Suman | BAH-286 | Fixed decimal value not showing in report for numeric type

--- src/main/java/org/bahmni/reports/template/SqlReportTemplate.java
+++ src/main/java/org/bahmni/reports/template/SqlReportTemplate.java
@@ -67,7 +67,6 @@ public class SqlReportTemplate extends BaseReportTemplate<SqlReportConfig> {
             case Types.TINYINT:
             case Types.SMALLINT:
             case Types.INTEGER:
-            case Types.NUMERIC:
                 return DataTypes.integerType();
 
             case Types.BIGINT:
