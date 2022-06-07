6858bd665818dd554098e1113042f23f62a7bf79
rohit-yawalkar <82825674+rohit-yawalkar@users.noreply.github.com>
2022-06-07 18:39:13 +0530

    BAH-1872 - | Rohit | - Removes file deletion code

--- src/main/java/org/bahmni/reports/filter/JasperResponseConverter.java
+++ src/main/java/org/bahmni/reports/filter/JasperResponseConverter.java
@@ -53,10 +53,6 @@ public class JasperResponseConverter {
                 exporterBuilder.setWorkbookTemplate(macroTemplateFile.toString());
                 exporterBuilder.addSheetName("Report");
                 concatenatedReportBuilder.toXls(exporterBuilder);
-                boolean delete = templateFile.delete();
-                if (!delete) {
-                    logger.warn(String.format("Uploaded report template file not deleted: %s", macroTemplateFile));
-                }
                 break;
             case APPLICATION_PDF:
                 concatenatedReportBuilder.toPdf(outputStream);
