9dd6b9a28ac8df0da56dfa843e5b209261c45e87
binduak <tbindu@thoughtworks.com>
2022-11-07 17:32:28 +0530

    Bindu | BAH-2490 | fix failing build issue

--- package/resources/run-liquibase.sh
+++ package/resources/run-liquibase.sh
@@ -4,9 +4,9 @@ set -e -x
 
 
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=$3"
-LIQUIBASE_JAR="${WAR_DIRECTORY}/WEB-INF/lib/liquibase-core-2.0.5.jar"
-DRIVER="com.mysql.jdbc.Driver"
-CLASSPATH="${WAR_DIRECTORY}/WEB-INF/lib/mysql-connector-java-5.1.26.jar"
+LIQUIBASE_JAR="${WAR_DIRECTORY}/WEB-INF/lib/liquibase-core-4.8.0.jar"
+DRIVER="com.mysql.cj.jdbc.Driver"
+CLASSPATH="${WAR_DIRECTORY}/WEB-INF/lib/mysql-connector-java-8.0.26.jar"
 CHANGE_LOG_FILE="$1"
 
 (cd ${WAR_DIRECTORY}/WEB-INF/classes/ && java $CHANGE_LOG_TABLE  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=$CHANGE_LOG_FILE --url=jdbc:mysql://$2:3306/$3 --username=$4 --password=$5 update)
