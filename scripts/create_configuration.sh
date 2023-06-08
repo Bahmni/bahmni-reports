308cf7f707e02e1b5859ffc0e7d6d083a0e2b01f
Rahul Ramesh <121226043+rahu1ramesh@users.noreply.github.com>
2023-06-08 15:20:58 +0530

    Revert "BAH-2131|Report-generated-on time-is-incorrect-in-report"
    This reverts commit d05e8bc3466c05aa5a27216a7aad6eb01a8bcb8e.

--- scripts/create_configuration.sh
+++ scripts/create_configuration.sh
@@ -20,7 +20,6 @@ openmrs.service.password=test
 openmrs.connectionTimeoutInMilliseconds=30000
 openmrs.replyTimeoutInMilliseconds=120000
 macrotemplates.temp.directory=/tmp/
-reports.timezone=Asia/Kolkata
 bahmnireports.db.url=jdbc:mysql://localhost:3306/bahmni_reports?allowMultiQueries=true
 reports.save.directory=$TARGET_DIR/reports""" > $TARGET_DIR/bahmni-reports.properties
 
@@ -40,7 +39,6 @@ openmrs.service.password=test
 openmrs.connectionTimeoutInMilliseconds=30000
 openmrs.replyTimeoutInMilliseconds=120000
 macrotemplates.temp.directory=/tmp/
-reports.timezone=Asia/Kolkata
 bahmnireports.db.url=jdbc:mysql://localhost:3306/bahmni_reports_it?allowMultiQueries=true
 reports.save.directory=$TARGET_DIR/reports""" > $TARGET_DIR/bahmni-reports-test.properties
 
