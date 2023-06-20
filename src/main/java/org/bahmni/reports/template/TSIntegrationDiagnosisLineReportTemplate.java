90c3bdf01f41c8a8a6c712a4d8a1a45fae0b8fef
manimaarans <manimaaran@beehyv.com>
2023-06-20 17:42:17 +0530

    BS-54 | added patient attributes and patient address attributes

--- src/main/java/org/bahmni/reports/template/TSIntegrationDiagnosisLineReportTemplate.java
+++ src/main/java/org/bahmni/reports/template/TSIntegrationDiagnosisLineReportTemplate.java
@@ -3,34 +3,45 @@ package org.bahmni.reports.template;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
 import net.sf.dynamicreports.report.constant.HorizontalAlignment;
 import net.sf.dynamicreports.report.constant.PageType;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bahmni.reports.model.GenericReportsConfig;
 import org.bahmni.reports.model.Report;
 import org.bahmni.reports.model.TSIntegrationDiagnosisLineReportConfig;
 import org.bahmni.reports.model.TSPageObject;
 import org.bahmni.reports.model.UsingDatasource;
 import org.bahmni.reports.report.BahmniReportBuilder;
 import org.bahmni.reports.util.CommonComponents;
+import org.bahmni.reports.util.SqlUtil;
 import org.bahmni.webclients.HttpClient;
+import org.quartz.impl.jdbcjobstore.InvalidConfigurationException;
 import org.stringtemplate.v4.ST;
 
 import java.io.IOException;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import static net.sf.dynamicreports.report.builder.DynamicReports.col;
 import static net.sf.dynamicreports.report.builder.DynamicReports.type;
 import static org.bahmni.reports.template.Templates.columnStyle;
+import static org.bahmni.reports.template.Templates.minimalColumnStyle;
 import static org.bahmni.reports.util.FileReaderUtil.getFileContent;
+import static org.bahmni.reports.util.GenericReportsHelper.constructPatientAddressesToDisplay;
+import static org.bahmni.reports.util.GenericReportsHelper.constructPatientAttributeNamesToDisplay;
+import static org.bahmni.reports.util.GenericReportsHelper.createAndAddPatientAddressColumns;
+import static org.bahmni.reports.util.GenericReportsHelper.createAndAddPatientAttributeColumns;
 
 
 @UsingDatasource("openmrs")
@@ -62,7 +73,7 @@ public class TSIntegrationDiagnosisLineReportTemplate extends BaseReportTemplate
     }
 
     @Override
-    public BahmniReportBuilder build(Connection connection, JasperReportBuilder jasperReport, Report<TSIntegrationDiagnosisLineReportConfig> report, String startDate, String endDate, List<AutoCloseable> resources, PageType pageType) {
+    public BahmniReportBuilder build(Connection connection, JasperReportBuilder jasperReport, Report<TSIntegrationDiagnosisLineReportConfig> report, String startDate, String endDate, List<AutoCloseable> resources, PageType pageType) throws SQLException, InvalidConfigurationException {
         String tempTableName = "tmpCodes_" + System.nanoTime();
         loadTempTable(connection, tempTableName, report.getConfig().getTerminologyParentCode());
         String sql = getFileContent("sql/tsIntegrationDiagnosisLineReport.sql");
@@ -79,8 +90,10 @@ public class TSIntegrationDiagnosisLineReportTemplate extends BaseReportTemplate
             jasperReport.addColumn(col.column(terminologyColumnName, TERMINOLOGY_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
         }
         jasperReport.addColumn(col.column(DATE_AND_TIME_COLUMN_NAME, DATE_AND_TIME_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
-        String formattedSql = getFormattedSql(sql, report.getConfig().getTsConceptSource(), startDate, endDate, tempTableName);
-        jasperReport.setDataSource(formattedSql, connection);
+        createAndAddPatientAttributeColumns(jasperReport, report.getConfig());
+        createAndAddPatientAddressColumns(jasperReport, report.getConfig());
+        ResultSet formattedSql = getFormattedSql(sql, report.getConfig().getTsConceptSource(), startDate, endDate, tempTableName, report.getConfig(), connection);
+        jasperReport.setDataSource(formattedSql);
 
         return new BahmniReportBuilder(jasperReport);
     }
@@ -128,13 +141,15 @@ public class TSIntegrationDiagnosisLineReportTemplate extends BaseReportTemplate
     }
 
 
-    private String getFormattedSql(String templateSql, String conceptSourceCode, String startDate, String endDate, String tempTableName) {
+    private ResultSet getFormattedSql(String templateSql, String conceptSourceCode, String startDate, String endDate, String tempTableName, TSIntegrationDiagnosisLineReportConfig config, Connection connection) throws SQLException, InvalidConfigurationException {
         ST sqlTemplate = new ST(templateSql, '#', '#');
         sqlTemplate.add("conceptSourceCode", conceptSourceCode);
         sqlTemplate.add("startDate", startDate);
         sqlTemplate.add("endDate", endDate);
         sqlTemplate.add("tempTable", tempTableName);
-        return sqlTemplate.render();
+        sqlTemplate.add("patientAttributes", constructPatientAttributeNamesToDisplay(config));
+        sqlTemplate.add("patientAddresses", constructPatientAddressesToDisplay(config));
+        return SqlUtil.executeSqlWithStoredProc(connection, sqlTemplate.render());
     }
 
 
@@ -147,4 +162,44 @@ public class TSIntegrationDiagnosisLineReportTemplate extends BaseReportTemplate
         return TS_DIAGNOSIS_LOOKUP_DEFAULT_PAGE_SIZE;
     }
 
+    public static void createAndAddPatientAddressColumns(JasperReportBuilder jasperReport, TSIntegrationDiagnosisLineReportConfig config) {
+        for (String address : getPatientAddresses(config)) {
+            TextColumnBuilder<String> column = col.column(address, address, type.stringType()).setStyle(minimalColumnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER);
+            jasperReport.addColumn(column);
+        }
+    }
+    public static void createAndAddPatientAttributeColumns(JasperReportBuilder jasperReport, TSIntegrationDiagnosisLineReportConfig config) {
+        for (String patientAttribute : getPatientAttributes(config)) {
+            TextColumnBuilder<String> column = col.column(patientAttribute, patientAttribute, type.stringType()).setStyle(minimalColumnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER);
+            jasperReport.addColumn(column);
+        }
+    }
+
+    private static List<String> getPatientAttributes(TSIntegrationDiagnosisLineReportConfig config) {
+        return config.getPatientAttributes() != null ? config.getPatientAttributes() : new ArrayList<String>();
+    }
+    private static List<String> getPatientAddresses(TSIntegrationDiagnosisLineReportConfig config) {
+        return config.getPatientAddresses() != null ? config.getPatientAddresses() : new ArrayList<String>();
+    }
+    public static String constructPatientAttributeNamesToDisplay(TSIntegrationDiagnosisLineReportConfig config) {
+        List<String> patientAttributes = getPatientAttributes(config);
+        List<String> parts = new ArrayList<>();
+        String helperString = "GROUP_CONCAT(DISTINCT(IF(pat.name = \\'%s\\', IF(pat.format = \\'org.openmrs.Concept\\',coalesce(scn.name, fscn.name),pa.value), NULL))) AS \\'%s\\'";
+
+        for (String patientAttribute : patientAttributes) {
+            parts.add(String.format(helperString, patientAttribute.replace("'", "\\\\\\\'"), patientAttribute.replace("'", "\\\\\\\'")));
+        }
+
+        return StringUtils.join(parts, ", ");
+    }
+    public static String constructPatientAddressesToDisplay(TSIntegrationDiagnosisLineReportConfig config) {
+        List<String> patientAddresses = getPatientAddresses(config);
+        StringBuilder stringBuilder = new StringBuilder();
+        if (patientAddresses != null) {
+            for (String address : patientAddresses) {
+                stringBuilder.append("paddress").append(".").append(address).append(", ");
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
