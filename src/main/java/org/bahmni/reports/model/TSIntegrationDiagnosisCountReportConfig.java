85316caecbf71e9788a88f0ca21de1681a8550ab
manimaarans <manimaaran@beehyv.com>
2023-06-23 11:11:29 +0530

    BS 54 | renamed existing count report files name to show the intent

--- src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
+++ src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
@@ -2,16 +2,57 @@ package org.bahmni.reports.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TSIntegrationDiagnosisCountReportConfig extends TSIntegrationDiagnosisReportConfig implements Config {
-    private Boolean displayGenderGroup;
+<<<<<<<< HEAD:src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
+public class TSIntegrationDiagnosisCountReportConfig implements Config {
+========
+public class TSIntegrationDiagnosisReportConfig {
+>>>>>>>> e1ab372 (BS 54 | renamed existing count report files name to show the intent):src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisReportConfig.java
+    private String tsConceptSource;
+    private String conceptNameDisplayFormat;
+    private String terminologyParentCode;
+    private String terminologyColumnName;
+    private Boolean displayTerminologyCode;
 
-    public Boolean isDisplayGenderGroup() {
-        return displayGenderGroup;
+    public String getTsConceptSource() {
+        return tsConceptSource;
     }
 
-    public void setDisplayGenderGroup(Boolean displayGenderGroup) {
-        this.displayGenderGroup = displayGenderGroup;
+    public void setTsConceptSource(String tsConceptSource) {
+        this.tsConceptSource = tsConceptSource;
     }
+
+    public String getConceptNameDisplayFormat() {
+        return conceptNameDisplayFormat;
+    }
+
+    public void setConceptNameDisplayFormat(String conceptNameDisplayFormat) {
+        this.conceptNameDisplayFormat = conceptNameDisplayFormat;
+    }
+
+    public String getTerminologyParentCode() {
+        return terminologyParentCode;
+    }
+
+    public void setTerminologyParentCode(String terminologyParentCode) {
+        this.terminologyParentCode = terminologyParentCode;
+    }
+
+    public Boolean isDisplayTerminologyCode() {
+        return displayTerminologyCode;
+    }
+
+    public void setDisplayTerminologyCode(Boolean displayTerminologyCode) {
+        this.displayTerminologyCode = displayTerminologyCode;
+    }
+
+    public String getTerminologyColumnName() {
+        return terminologyColumnName;
+    }
+
+    public void setTerminologyColumnName(String terminologyColumnName) {
+        this.terminologyColumnName = terminologyColumnName;
+    }
+
+
 }
--- src/main/java/org/bahmni/reports/report/TSIntegrationDiagnosisCountReport.java
+++ src/main/java/org/bahmni/reports/report/TSIntegrationDiagnosisCountReport.java
@@ -1,23 +1,61 @@
 package org.bahmni.reports.report;
 
-
+import org.apache.http.config.Registry;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bahmni.reports.BahmniReportsConfiguration;
 import org.bahmni.reports.BahmniReportsProperties;
+<<<<<<< HEAD:src/main/java/org/bahmni/reports/report/TSHttpClient.java
+=======
 import org.bahmni.reports.model.Report;
 import org.bahmni.reports.model.TSIntegrationDiagnosisCountReportConfig;
 import org.bahmni.reports.template.BaseReportTemplate;
 import org.bahmni.reports.template.TSIntegrationDiagnosisCountReportTemplate;
+>>>>>>> e1ab372 (BS 54 | renamed existing count report files name to show the intent):src/main/java/org/bahmni/reports/report/TSIntegrationDiagnosisCountReport.java
 import org.bahmni.webclients.HttpClient;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
-public class TSIntegrationDiagnosisCountReport extends Report<TSIntegrationDiagnosisCountReportConfig> implements TSHttpClient {
+<<<<<<< HEAD:src/main/java/org/bahmni/reports/report/TSHttpClient.java
+public interface TSHttpClient {
+    Logger logger = LogManager.getLogger(TSHttpClient.class);
+    final static String TS_PROPERTIES_FILENAME = "terminology-service-config.properties";
 
+    default HttpClient getHttpClient(BahmniReportsProperties bahmniReportsProperties) {
+=======
+public class TSIntegrationDiagnosisCountReport extends Report<TSIntegrationDiagnosisCountReportConfig> {
+    private static final Logger logger = LogManager.getLogger(TSIntegrationDiagnosisCountReport.class);
+    private static final String TS_PROPERTIES_FILENAME = "terminology-service-config.properties";
 
     @Override
     public BaseReportTemplate getTemplate(BahmniReportsProperties bahmniReportsProperties) {
         HttpClient httpClient = getHttpClient(bahmniReportsProperties);
         Properties tsProperties = getTSProperties();
-        String tsEndpointTemplate = bahmniReportsProperties.getOpenmrsRootUrl() + tsProperties.getProperty("terminologyServer.endpoint");
+        String tsEndpointTemplate = bahmniReportsProperties.getOpenmrsRootUrl() + tsProperties.getProperty("ts.endpoint");
         return new TSIntegrationDiagnosisCountReportTemplate(httpClient, tsProperties, tsEndpointTemplate);
+    }
+
+    private static HttpClient getHttpClient(BahmniReportsProperties bahmniReportsProperties) {
+>>>>>>> e1ab372 (BS 54 | renamed existing count report files name to show the intent):src/main/java/org/bahmni/reports/report/TSIntegrationDiagnosisCountReport.java
+        BahmniReportsConfiguration bahmniReportsConfiguration = new BahmniReportsConfiguration(bahmniReportsProperties);
+        SSLConnectionSocketFactory allTrustSSLSocketFactory = bahmniReportsConfiguration.allTrustSSLSocketFactory();
+        Registry<ConnectionSocketFactory> schemeRegistry = bahmniReportsConfiguration.schemeRegistry(allTrustSSLSocketFactory);
+        HttpClient httpClient = bahmniReportsConfiguration.httpClient(schemeRegistry);
+        return httpClient;
+    }
+
+    default Properties getTSProperties() {
+        try (InputStream in = this.getClass().getClassLoader().getResourceAsStream(TS_PROPERTIES_FILENAME)) {
+            Properties p = new Properties();
+            p.load(in);
+            return p;
+        } catch (IOException e) {
+            logger.error("Could not load terminology service properties from: " + TS_PROPERTIES_FILENAME, e);
+            throw new RuntimeException();
+        }
     }
 }
