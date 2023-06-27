d14ec6f150c7e2b7acf57df923fa03c145550715
manimaarans <manimaaran@beehyv.com>
2023-06-27 14:57:16 +0530

    BS 54 | extracted common properties to generic and extend them for count and line report

--- src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
+++ src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
@@ -2,57 +2,16 @@ package org.bahmni.reports.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-<<<<<<<< HEAD:src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisCountReportConfig.java
-public class TSIntegrationDiagnosisCountReportConfig implements Config {
-========
-public class TSIntegrationDiagnosisReportConfig {
->>>>>>>> e1ab372 (BS 54 | renamed existing count report files name to show the intent):src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisReportConfig.java
-    private String tsConceptSource;
-    private String conceptNameDisplayFormat;
-    private String terminologyParentCode;
-    private String terminologyColumnName;
-    private Boolean displayTerminologyCode;
+public class TSIntegrationDiagnosisCountReportConfig extends TSIntegrationDiagnosisReportConfig implements Config {
+    private Boolean displayGenderGroup;
 
-    public String getTsConceptSource() {
-        return tsConceptSource;
+    public Boolean isDisplayGenderGroup() {
+        return displayGenderGroup;
     }
 
-    public void setTsConceptSource(String tsConceptSource) {
-        this.tsConceptSource = tsConceptSource;
+    public void setDisplayGenderGroup(Boolean displayGenderGroup) {
+        this.displayGenderGroup = displayGenderGroup;
     }
-
-    public String getConceptNameDisplayFormat() {
-        return conceptNameDisplayFormat;
-    }
-
-    public void setConceptNameDisplayFormat(String conceptNameDisplayFormat) {
-        this.conceptNameDisplayFormat = conceptNameDisplayFormat;
-    }
-
-    public String getTerminologyParentCode() {
-        return terminologyParentCode;
-    }
-
-    public void setTerminologyParentCode(String terminologyParentCode) {
-        this.terminologyParentCode = terminologyParentCode;
-    }
-
-    public Boolean isDisplayTerminologyCode() {
-        return displayTerminologyCode;
-    }
-
-    public void setDisplayTerminologyCode(Boolean displayTerminologyCode) {
-        this.displayTerminologyCode = displayTerminologyCode;
-    }
-
-    public String getTerminologyColumnName() {
-        return terminologyColumnName;
-    }
-
-    public void setTerminologyColumnName(String terminologyColumnName) {
-        this.terminologyColumnName = terminologyColumnName;
-    }
-
-
 }
--- src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisLineReportConfig.java
+++ src/main/java/org/bahmni/reports/model/TSIntegrationDiagnosisLineReportConfig.java
@@ -6,55 +6,9 @@ import java.util.List;
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TSIntegrationDiagnosisLineReportConfig implements Config {
-    private String tsConceptSource;
-    private String conceptNameDisplayFormat;
-    private String terminologyParentCode;
-    private String terminologyColumnName;
-    private Boolean displayTerminologyCode;
+public class TSIntegrationDiagnosisLineReportConfig extends TSIntegrationDiagnosisReportConfig implements Config {
     private List<String> patientAttributes;
     private List<String> patientAddresses;
-
-    public String getTsConceptSource() {
-        return tsConceptSource;
-    }
-
-    public void setTsConceptSource(String tsConceptSource) {
-        this.tsConceptSource = tsConceptSource;
-    }
-
-    public String getConceptNameDisplayFormat() {
-        return conceptNameDisplayFormat;
-    }
-
-    public void setConceptNameDisplayFormat(String conceptNameDisplayFormat) {
-        this.conceptNameDisplayFormat = conceptNameDisplayFormat;
-    }
-
-    public String getTerminologyParentCode() {
-        return terminologyParentCode;
-    }
-
-    public void setTerminologyParentCode(String terminologyParentCode) {
-        this.terminologyParentCode = terminologyParentCode;
-    }
-
-    public Boolean isDisplayTerminologyCode() {
-        return displayTerminologyCode;
-    }
-
-    public void setDisplayTerminologyCode(Boolean displayTerminologyCode) {
-        this.displayTerminologyCode = displayTerminologyCode;
-    }
-
-    public String getTerminologyColumnName() {
-        return terminologyColumnName;
-    }
-
-    public void setTerminologyColumnName(String terminologyColumnName) {
-        this.terminologyColumnName = terminologyColumnName;
-    }
-
     public List<String> getPatientAttributes() {
         return patientAttributes;
     }
