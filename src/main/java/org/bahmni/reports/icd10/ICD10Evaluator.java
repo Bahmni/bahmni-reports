cab84ae606add19e430b929d0d4a45b51967d869
vijayanandtwks <vijayanand.s@thoughtworks.com>
2023-06-28 14:53:38 +0530

    BS - 174 | Vijay,Mani | ICD-10 rule evaluation logic

--- src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
+++ src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bahmni.reports.icd10.bean.ICDRule;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import javax.script.*;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -13,13 +12,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ICD10Evaluator {
-    ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-    ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+    static ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+    static ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
     static List<ICDRule> rules = new ArrayList<>();
-    int age = 40;
-    String gender = "F";
+
 
     public static void main(String[] args) {
+        int age = 40;
+        String gender = "F";
+
         try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("icd-response2.json")) {
             ObjectMapper mapper = new ObjectMapper();
             //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -42,8 +43,9 @@ public class ICD10Evaluator {
                 }
             };
             List<ICDRule> sortedRules =  rules.stream().sorted(customComparator).collect(Collectors.toList());
+            System.out.println("-----------------------------");
             sortedRules.forEach(System.out::println);
-            /*
+            System.out.println("-----------------------------");
             String currentMapGroup = "1";
             boolean isMapTargetFound = false;
             List<String> mapTargets = new ArrayList<>();
@@ -52,31 +54,41 @@ public class ICD10Evaluator {
                     currentMapGroup = rule.getMapGroup();
                     isMapTargetFound = false;
                 } else if (isMapTargetFound) continue;
-                String mapTarget = evaluate(rule.getMapRule());
+                String mapTarget = evaluate(rule,age,gender);
                 if (mapTarget != null) {
                     mapTargets.add(mapTarget);
                     isMapTargetFound = true;
                 }
             }
-             */
-
-
-
+            System.out.println("-----------------------------");
+            mapTargets.stream().forEach(System.out::println);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    /*
-    private static String evaluate(String mapRule) {
-        String executableMapRule = mapRule.replace("IFA 248153007 | Male (finding) |", "gender=='M'")
+
+    private static String evaluate(ICDRule rule, int age, String gender) {
+        String mapRule = rule.getMapRule();
+        String expression = mapRule.replace("IFA 248153007 | Male (finding) |", "gender=='M'")
                 .replace("IFA 248152002 | Female (finding) |", "gender=='F'")
                 .replace("IFA 445518008 | Age at onset of clinical finding (observable entity) |", "age")
+                .replace("OTHERWISE TRUE", "true")
+                .replace("TRUE", "true")
                 .replace("AND", "&&")
                 .replace("OR", "||")
                 .replace(" years", "");
-        System.out.println(executableMapRule);
+        System.out.println(expression);
+        try {
+            Bindings bindings = new SimpleBindings();
+            bindings.put("age", age);
+            bindings.put("gender", gender);
+            Boolean result = (Boolean) scriptEngine.eval(expression,bindings);
+            if(result) return rule.getMapTarget();
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
-     */
+
 }
