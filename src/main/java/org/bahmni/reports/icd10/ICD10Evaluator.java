42217813ae895dcc97b81a1e74c9203e823fac15
vijayanandtwks <vijayanand.s@thoughtworks.com>
2023-06-28 13:38:11 +0530

    BS - 174 | Vijay,Mani | ICD-10 sorting logic

--- src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
+++ src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
@@ -1,22 +1,28 @@
 package org.bahmni.reports.icd10;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bahmni.reports.icd10.bean.ICDRule;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class ICD10Evaluator {
+    ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+    ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
     static List<ICDRule> rules = new ArrayList<>();
+    int age = 40;
+    String gender = "F";
+
     public static void main(String[] args) {
-        try(InputStream in=Thread.currentThread().getContextClassLoader().getResourceAsStream("icd-response2.json")){
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("icd-response2.json")) {
             ObjectMapper mapper = new ObjectMapper();
+            //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             JsonNode jsonNode = mapper.readValue(in, JsonNode.class);
             JsonNode itemsArr = jsonNode.get("items");
             if (itemsArr.isArray()) {
@@ -25,15 +31,52 @@ public class ICD10Evaluator {
                     rules.add(rule);
                 }
             }
+            Comparator<ICDRule> customComparator = new Comparator<ICDRule>() {
+                @Override
+                public int compare(ICDRule rule1, ICDRule rule2) {
+                    int rule1Weight = Integer.parseInt(rule1.mapGroup + rule1.mapPriority);
+                    int rule2Weight = Integer.parseInt(rule2.mapGroup + rule2.mapPriority);
+                    int difference = rule1Weight - rule2Weight;
+                    System.out.println(rule1Weight + " " + rule2Weight + " " + difference);
+                    return difference;
+                }
+            };
+            List<ICDRule> sortedRules =  rules.stream().sorted(customComparator).collect(Collectors.toList());
+            sortedRules.forEach(System.out::println);
+            /*
+            String currentMapGroup = "1";
+            boolean isMapTargetFound = false;
+            List<String> mapTargets = new ArrayList<>();
+            for (ICDRule rule : sortedRules) {
+                if (!currentMapGroup.equals(rule.getMapGroup())) {
+                    currentMapGroup = rule.getMapGroup();
+                    isMapTargetFound = false;
+                } else if (isMapTargetFound) continue;
+                String mapTarget = evaluate(rule.getMapRule());
+                if (mapTarget != null) {
+                    mapTargets.add(mapTarget);
+                    isMapTargetFound = true;
+                }
+            }
+             */
 
-            //List<ICDRule> rules = mapper.readValue(mapper.writeValueAsString(items), new TypeReference<List<ICDRule>>(){});
 
-            //String jsonString = mapper.writeValueAsString(jsonNode);
-            //System.out.println(jsonString);
-            System.out.println(rules);
-        }
-        catch(Exception e){
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    /*
+    private static String evaluate(String mapRule) {
+        String executableMapRule = mapRule.replace("IFA 248153007 | Male (finding) |", "gender=='M'")
+                .replace("IFA 248152002 | Female (finding) |", "gender=='F'")
+                .replace("IFA 445518008 | Age at onset of clinical finding (observable entity) |", "age")
+                .replace("AND", "&&")
+                .replace("OR", "||")
+                .replace(" years", "");
+        System.out.println(executableMapRule);
+        return null;
+    }
+     */
 }
--- src/main/java/org/bahmni/reports/icd10/bean/ICDRule.java
+++ src/main/java/org/bahmni/reports/icd10/bean/ICDRule.java
@@ -3,25 +3,15 @@ package org.bahmni.reports.icd10.bean;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ICDRule /* implements Comparable<ICDRule> */{
+public class ICDRule {
     public String mapPriority;
     public String mapGroup;
     public String mapRule;
     public String mapTarget;
 
-    /*
-    @Override
-    public int compareTo(ICDRule anotherRule) {
-        String groupDifference = Integer.toString(Integer.parseInt(this.mapGroup) - Integer.parseInt(anotherRule.mapGroup));
-        String prioityDifference = Integer.toString(Integer.parseInt(this.mapPriority) - Integer.parseInt(anotherRule.mapPriority));
-        String totalDifference = groupDifference+prioityDifference;
-        return Integer.parseInt(totalDifference);
-    }
-     */
-
     @Override
     public String toString() {
-        return this.mapGroup+" "+this.mapPriority+" "+this.mapRule;
+        return this.mapGroup + " " + this.mapPriority + " " + this.mapRule;
     }
 
     public String getMapPriority() {
