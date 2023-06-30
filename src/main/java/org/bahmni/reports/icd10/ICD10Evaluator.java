230e54521d60d99ee1ce7de5e45041cd8f71ebca
manimaarans <manimaaran@beehyv.com>
2023-06-30 10:55:13 +0530

    BS 174 | Vijay, Mani | updated comparator logic and added eval logic
    # Conflicts:
    #	src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
    #	src/main/resources/icd-response2.json

--- src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
+++ src/main/java/org/bahmni/reports/icd10/ICD10Evaluator.java
@@ -1,20 +1,43 @@
 package org.bahmni.reports.icd10;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.bahmni.reports.icd10.bean.ICDRule;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.script.*;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ICD10Evaluator {
     static ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
     static ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("Nashorn");
     static List<ICDRule> rules = new ArrayList<>();
+    static final String TRUE_KEYWORD = "TRUE";
+    static final String FALSE_KEYWORD = "FALSE";
+    static final String OTHERWISE_TRUE_KEYWORD = "OTHERWISE TRUE";
+    static final String MALE_KEYWORD = "IFA 248153007 | Male (finding) |";
+    static final String FEMALE_KEYWORD = "IFA 248152002 | Female (finding) |";
+    static final String AGE_KEYWORD = "IFA 445518008 | Age at onset of clinical finding (observable entity) |";
+    static final String AND_KEYWORD = "AND";
+    static final String AND_KEYWORD_REPLACE = "&&";
+    static final String OR_KEYWORD = "OR";
+    static final String OR_KEYWORD_REPLACE = "||";
+    static final String YEARS_KEYWORD = " years";
+    static final String YEARS_KEYWORD_REPLACE= "";
+    int age = 40;
+    String gender = "F";
 
 
     public static void main(String[] args) {
@@ -23,12 +46,13 @@ public class ICD10Evaluator {
         ICD10Evaluator icd10Evaluator = new ICD10Evaluator();
         String icdCodes = icd10Evaluator.getICDCodes("", age, gender);
 
+
     }
 
     public String getICDCodes(String snomedCode, int age, String gender){
         try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("icd-response2.json")) {
             ObjectMapper mapper = new ObjectMapper();
-            //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             JsonNode jsonNode = mapper.readValue(in, JsonNode.class);
             JsonNode itemsArr = jsonNode.get("items");
             if (itemsArr.isArray()) {
@@ -40,17 +64,26 @@ public class ICD10Evaluator {
             Comparator<ICDRule> customComparator = new Comparator<ICDRule>() {
                 @Override
                 public int compare(ICDRule rule1, ICDRule rule2) {
-                    int rule1Weight = Integer.parseInt(rule1.mapGroup + rule1.mapPriority);
-                    int rule2Weight = Integer.parseInt(rule2.mapGroup + rule2.mapPriority);
-                    int difference = rule1Weight - rule2Weight;
-                    System.out.println(rule1Weight + " " + rule2Weight + " " + difference);
-                    return difference;
+                    if (Integer.parseInt(rule1.mapGroup) < Integer.parseInt(rule2.mapGroup)) {
+                        return -1;
+                    }
+                    if (Integer.parseInt(rule1.mapGroup) > Integer.parseInt(rule2.mapGroup)) {
+                        return 1;
+                    }
+                    if (Integer.parseInt(rule1.mapPriority) < Integer.parseInt(rule2.mapPriority)) {
+                        return -1;
+                    }
+                    if (Integer.parseInt(rule1.mapPriority) > Integer.parseInt(rule2.mapPriority)) {
+                        return 1;
+                    }
+                    return Integer.compare(Integer.parseInt(rule1.mapTarget), Integer.parseInt(rule2.mapTarget));
                 }
             };
             List<ICDRule> sortedRules =  rules.stream().sorted(customComparator).collect(Collectors.toList());
             System.out.println("-----------------------------");
             sortedRules.forEach(System.out::println);
             System.out.println("-----------------------------");
+            /*
             String currentMapGroup = "1";
             boolean isMapTargetFound = false;
             List<String> mapTargets = new ArrayList<>();
@@ -70,7 +103,39 @@ public class ICD10Evaluator {
             return String.join(",", mapTargets);
         } catch (Exception e) {
             throw new RuntimeException(e);
+             */
+
+
+            //String jsonString = mapper.writeValueAsString(jsonNode);
+            //System.out.println(jsonString);
+            int patientAge = 34;
+            String patientGender = "F";
+            List<String> selectedCodes = new ArrayList<>();
+            String mapGroup = "";
+            boolean isFound = false;
+            for(ICDRule rule : sortedRules) {
+                if (!mapGroup.equalsIgnoreCase(rule.getMapGroup())) {
+                    mapGroup = rule.getMapGroup();
+                    isFound = false;
+                }
+                if(mapGroup.equalsIgnoreCase(rule.getMapGroup()) && isFound) {
+                    continue;
+                }
+                if(mapGroup.equalsIgnoreCase(rule.getMapGroup()) && !isFound) {
+                    if(evaluateRule(convertRule(rule.getMapRule(), patientAge, patientGender))) {
+                        selectedCodes.add(rule.getMapTarget());
+                        isFound =true;
+                    }
+                }
+            }
+            System.out.println(selectedCodes);
+            return selectedCodes.stream().filter(StringUtils::isNotBlank).collect(Collectors.joining(","));
+
         }
+        catch(Exception e){
+
+        }
+        return "";
     }
 
 
@@ -97,4 +162,24 @@ public class ICD10Evaluator {
         return null;
     }
 
+    static String convertRule(String rule, Integer age, String gender) {
+        rule = rule.replace(OTHERWISE_TRUE_KEYWORD, TRUE_KEYWORD);
+        rule = rule.replace(OTHERWISE_TRUE_KEYWORD, TRUE_KEYWORD);
+        rule = rule.replace(MALE_KEYWORD, gender.equalsIgnoreCase("M") ? TRUE_KEYWORD : FALSE_KEYWORD );
+        rule = rule.replace(FEMALE_KEYWORD, gender.equalsIgnoreCase("F") ? TRUE_KEYWORD : FALSE_KEYWORD );
+        rule = rule.replace(AGE_KEYWORD, age.toString());
+        rule = rule.replace(AND_KEYWORD, AND_KEYWORD_REPLACE);
+        rule = rule.replace(OR_KEYWORD, OR_KEYWORD_REPLACE);
+        rule = rule.replace(YEARS_KEYWORD, YEARS_KEYWORD_REPLACE);
+        return rule;
+    }
+   static boolean evaluateRule(String rule) throws ScriptException {
+        if (rule.equalsIgnoreCase(TRUE_KEYWORD)) {
+            return true;
+        }
+       if (rule.equalsIgnoreCase(FALSE_KEYWORD)) {
+           return false;
+       }
+        return (boolean) scriptEngine.eval(rule);
+    }
 }
