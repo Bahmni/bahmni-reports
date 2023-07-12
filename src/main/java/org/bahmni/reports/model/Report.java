6d3d79632206da7880a9097d6872d48997949efe
Siva Reddy <sivareddy@outlook.com>
2023-07-12 23:19:04 +0530

    BS-91 | SNOMED master changes to product master (#68)
    * BS-39 | Siva,Vijay | Publish bahmni-reports with SNOMED image tags (#61)
    
    * Siva Reddy | BS-42 | deployment to SNOMED Environment (#64)
    
    * Siva Reddy | Triggering Deployment in new repo for SNOMED
    
    * Enhancing Reports to support SNOMED Feature  (#67)
    
    * BS-55 | David | Introduced snomedDiagnosis Report type and template
    
    * BS-55 | Vijay,David | Integrate Diagnosis Report with SNOMED omod
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * BS-55 | Vijay,David | Fix count error for Diagnosis Report
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * Testing HTTP URL Connection
    
    * BS - 55 | Vijay,David | Add gender report for SNOMED diagnosis count
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * BS - 55 | Vijay,David | Add unit tests for SNOMED diagnosis Count and Gender
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * BS - 55 | Vijay,David | Refactor implementation and unit tests for SNOMED diagnosis Count and Gender
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * BS - 55/51/24 | Vijay,David | Clean up
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * BS - 55/51/24 | Vijay | probe HttpClient issue
    
    * BS - 55/51/24 | Vijay | upgrade bahmniJavaUtils version
    
    * BS - 55/51/24 | Vijay | remove stale branch
    
    * BS - 55/51/24 | Vijay | remove feature branch
    
    * BS - 55/51/24 | Vijay | cleanup files
    
    * BS - 55/51/24 | Vijay | trigger pipeline
    
    * BS - 55/51/24 | Vijay | incorporate review comments
    
    * BS - 55/51/24 | Vijay | remove stale branch
    
    ---------
    
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>
    
    * remove unwanted line
    
    * Siva Reddy | BS-91 | Reverting changes made to snomed-master
    
    * BS - 113 | Vijay | add readme (#70)
    
    * daviemukungi | BS-138 | Align report values to fit at the center of a… (#71)
    
    Daviemukungi | BS-138 | Align report values to fit at the center of a cell. Also removed unused imports
    
    * BS - 141 | Vijay | add summary total and replace short name instead of FSN and rename Total to Count (#72)
    
    * BS - 166 | Vijay | fix for diagnosis count not showing results in clinic config (#75)
    
    * BS - 166 | Vijay | fix for diagnosis count not showing results in clinic config
    
    ---------
    
    Co-authored-by: vijayanandtwks <113415613+vijayanandtwks@users.noreply.github.com>
    Co-authored-by: daviemukungi <mukungidavid@gmail.com>

--- src/main/java/org/bahmni/reports/model/Report.java
+++ src/main/java/org/bahmni/reports/model/Report.java
@@ -55,7 +55,8 @@ import static org.bahmni.reports.model.Constants.*;
         @JsonSubTypes.Type(value = OpenmrsConcatenatedReport.class, name = "concatenated"),
         @JsonSubTypes.Type(value = MartConcatenatedReport.class, name = "MartConcatenated"),
         @JsonSubTypes.Type(value = GenericLabOrderReport.class, name = LABORDERS),
-        @JsonSubTypes.Type(value = ObservationFormReport.class, name = FORM_BUILDER_FORMS)
+        @JsonSubTypes.Type(value = ObservationFormReport.class, name = FORM_BUILDER_FORMS),
+        @JsonSubTypes.Type(value = TSIntegrationDiagnosisReport.class, name = FHIR_TS_LOOKUP_DIAGNOSIS)
 })
 
 public abstract class Report<T extends Config> {
---
+++ src/main/java/org/bahmni/reports/template/TSIntegrationDiagnosisReportTemplate.java
@@ -0,0 +1,165 @@
+package org.bahmni.reports.template;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
+import net.sf.dynamicreports.report.builder.style.StyleBuilder;
+import net.sf.dynamicreports.report.builder.subtotal.AggregationSubtotalBuilder;
+import net.sf.dynamicreports.report.constant.HorizontalAlignment;
+import net.sf.dynamicreports.report.constant.PageType;
+import net.sf.dynamicreports.report.constant.WhenNoDataType;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bahmni.reports.model.Report;
+import org.bahmni.reports.model.TSIntegrationDiagnosisReportConfig;
+import org.bahmni.reports.model.TSPageObject;
+import org.bahmni.reports.model.UsingDatasource;
+import org.bahmni.reports.report.BahmniReportBuilder;
+import org.bahmni.reports.util.CommonComponents;
+import org.bahmni.webclients.HttpClient;
+import org.stringtemplate.v4.ST;
+
+import java.io.IOException;
+import java.net.URI;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Properties;
+
+import static net.sf.dynamicreports.report.builder.DynamicReports.*;
+import static org.bahmni.reports.template.Templates.columnStyle;
+import static org.bahmni.reports.util.FileReaderUtil.getFileContent;
+
+
+@UsingDatasource("openmrs")
+public class TSIntegrationDiagnosisReportTemplate extends BaseReportTemplate<TSIntegrationDiagnosisReportConfig> {
+    public static final String CREATE_SQL_TEMPLATE = "create temporary table {0}(code varchar(100) not null)";
+    public static final String INSERT_SQL_TEMPLATE = "insert into {0} values (?)";
+    public static final String DIAGNOSIS_COLUMN_NAME = "Diagnosis";
+    public static final String TERMINOLOGY_COLUMN_NAME = "Terminology Code";
+    public static final String FEMALE_COLUMN_NAME = "Female";
+    public static final String MALE_COLUMN_NAME = "Male";
+    public static final String OTHER_COLUMN_NAME = "Other";
+    public static final String NOT_DISCLOSED_COLUMN_NAME = "Not disclosed";
+    public static final String TOTAL_COLUMN_NAME = "Total";
+    public static final String COUNT_COLUMN_NAME = "Count";
+    public static final String TOTAL_LABEL_NAME = "Total";
+    public static final int TS_DIAGNOSIS_LOOKUP_DEFAULT_PAGE_SIZE = 20;
+    public static final String SHORT_DISPLAY_FORMAT = "SHORT";
+    public static final String FULLY_SPECIFIED_DISPLAY_FORMAT = "FULLY_SPECIFIED";
+    private static final Logger logger = LogManager.getLogger(TSIntegrationDiagnosisReportTemplate.class);
+    private HttpClient httpClient;
+    private Properties tsProperties;
+    private String descendantsUrlTemplate;
+
+    public TSIntegrationDiagnosisReportTemplate(HttpClient httpClient, Properties tsProperties, String descendantsUrlTemplate) {
+        super();
+        this.httpClient = httpClient;
+        this.tsProperties = tsProperties;
+        this.descendantsUrlTemplate = descendantsUrlTemplate;
+    }
+
+    public void setDescendantsUrlTemplate(String descendantsUrlTemplate) {
+        this.descendantsUrlTemplate = descendantsUrlTemplate;
+    }
+
+    @Override
+    public BahmniReportBuilder build(Connection connection, JasperReportBuilder jasperReport, Report<TSIntegrationDiagnosisReportConfig> report, String startDate, String endDate, List<AutoCloseable> resources, PageType pageType) {
+        String tempTableName = "tmpCodes_" + System.nanoTime();
+        loadTempTable(connection, tempTableName, report.getConfig().getTerminologyParentCode());
+        String sql = getFileContent("sql/tsIntegrationDiagnosisCount.sql");
+
+        CommonComponents.addTo(jasperReport, report, pageType);
+        jasperReport.addColumn(col.column(DIAGNOSIS_COLUMN_NAME, DIAGNOSIS_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+        if (report.getConfig().isDisplayTerminologyCode()) {
+            String terminologyConfigColumnName = report.getConfig().getTerminologyColumnName();
+            String terminologyColumnName = StringUtils.isNotBlank(terminologyConfigColumnName) ? terminologyConfigColumnName : TERMINOLOGY_COLUMN_NAME;
+            jasperReport.addColumn(col.column(terminologyColumnName, TERMINOLOGY_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+        }
+        if (report.getConfig().isDisplayGenderGroup()) {
+            jasperReport.addColumn(col.column(FEMALE_COLUMN_NAME, FEMALE_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+            jasperReport.addColumn(col.column(MALE_COLUMN_NAME, MALE_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+            jasperReport.addColumn(col.column(OTHER_COLUMN_NAME, OTHER_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+            jasperReport.addColumn(col.column(NOT_DISCLOSED_COLUMN_NAME, NOT_DISCLOSED_COLUMN_NAME, type.stringType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER));
+        }
+        TextColumnBuilder<Integer> rowCount = col.column(COUNT_COLUMN_NAME, TOTAL_COLUMN_NAME, type.integerType()).setStyle(columnStyle).setHorizontalAlignment(HorizontalAlignment.CENTER);
+        jasperReport.addColumn(rowCount);
+        StyleBuilder subtotalStyle = stl.style().bold().setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        AggregationSubtotalBuilder<Integer> totalCount = sbt.sum(rowCount)
+                .setLabel(TOTAL_LABEL_NAME)
+                .setLabelStyle(subtotalStyle);
+        String formattedSql = getFormattedSql(sql, report.getConfig().getTsConceptSource(), report.getConfig().getConceptNameDisplayFormat(), startDate, endDate, tempTableName);
+        jasperReport.setShowColumnTitle(true).setWhenNoDataType(WhenNoDataType.ALL_SECTIONS_NO_DETAIL).subtotalsAtSummary(totalCount).setDataSource(formattedSql, connection);
+
+        return new BahmniReportBuilder(jasperReport);
+    }
+
+    private void loadTempTable(Connection connection, String tempTableName, String parentCode) {
+        int offset = 0;
+        int pageSize = getDefaultPageSize();
+        try {
+            String createSqlStmt = MessageFormat.format(CREATE_SQL_TEMPLATE, tempTableName);
+            String insertSqlStmt = MessageFormat.format(INSERT_SQL_TEMPLATE, tempTableName);
+            Statement statement = connection.createStatement();
+            statement.execute(createSqlStmt);
+            PreparedStatement pstmtInsert = connection.prepareStatement(insertSqlStmt);
+
+            TSPageObject pageObject = null;
+            do {
+                try {
+                    pageObject = fetchDescendantsByPagination(parentCode, pageSize, offset, "en");
+                } catch (IOException e) {
+                    throw new RuntimeException();
+                }
+                List<String> codes = pageObject.getCodes();
+                for (int batchcount = 0; batchcount < codes.size(); batchcount++) {
+                    pstmtInsert.setString(1, codes.get(batchcount));
+                    pstmtInsert.addBatch();
+                }
+                pstmtInsert.executeBatch();
+                offset += pageSize;
+            } while (offset < pageObject.getTotal());
+        } catch (SQLException e) {
+            logger.error("Error occured while making database call to " + tempTableName + " table");
+            throw new RuntimeException();
+        }
+    }
+
+    private TSPageObject fetchDescendantsByPagination(String terminologyCode, int pageSize, int offset, String localeLanguage) throws IOException {
+        String url = MessageFormat.format(descendantsUrlTemplate, terminologyCode, pageSize, offset, localeLanguage);
+        String responseStr = httpClient.get(URI.create(url));
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(responseStr, TSPageObject.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    private String getFormattedSql(String templateSql, String conceptSourceCode, String conceptNameDisplayFormat, String startDate, String endDate, String tempTableName) {
+        ST sqlTemplate = new ST(templateSql, '#', '#');
+        sqlTemplate.add("conceptSourceCode", conceptSourceCode);
+        sqlTemplate.add("startDate", startDate);
+        sqlTemplate.add("endDate", endDate);
+        sqlTemplate.add("tempTable", tempTableName);
+        sqlTemplate.add("conceptNameDisplayFormat", "shortNamePreferred".equals(conceptNameDisplayFormat) ? SHORT_DISPLAY_FORMAT : FULLY_SPECIFIED_DISPLAY_FORMAT);
+        return sqlTemplate.render();
+    }
+
+
+    public int getDefaultPageSize() {
+        String pageSize = System.getenv("REPORTS_TS_PAGE_SIZE");
+        if (pageSize == null) pageSize = tsProperties.getProperty("ts.defaultPageSize");
+        if (pageSize != null) {
+            return Integer.parseInt(pageSize);
+        }
+        return TS_DIAGNOSIS_LOOKUP_DEFAULT_PAGE_SIZE;
+    }
+
+}
