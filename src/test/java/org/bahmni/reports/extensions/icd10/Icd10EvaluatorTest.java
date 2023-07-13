5672913b098fa0f8d53ca99bdbbebf806b0a9720
vijayanandtwks <vijayanand.s@thoughtworks.com>
2023-07-13 12:24:24 +0530

    BS-174 | Vijay | refactored test cases

--- src/test/java/org/bahmni/reports/extensions/icd10/Icd10EvaluatorTest.java
+++ src/test/java/org/bahmni/reports/extensions/icd10/Icd10EvaluatorTest.java
@@ -1,10 +1,10 @@
 package org.bahmni.reports.extensions.icd10;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bahmni.reports.extensions.icd10.Impl.Icd10LookupServiceImpl;
-import org.bahmni.reports.extensions.icd10.bean.ICDResponse;
-import org.bahmni.reports.extensions.icd10.bean.ICDRule;
+import org.bahmni.reports.extensions.icd10.Impl.Icd10LookupServiceImplTest;
+import org.bahmni.reports.extensions.icd10.bean.IcdResponse;
+import org.bahmni.reports.extensions.icd10.bean.IcdRule;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,12 +15,6 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,7 +28,6 @@ public class Icd10EvaluatorTest {
     Icd10Evaluator icd10Evaluator;
     @Mock
     Icd10LookupServiceImpl icd10Service;
-    ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
     public void setUp() {
@@ -43,7 +36,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_ForSingleMapGroup() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_SingleMapGroup.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_SingleMapGroup.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "M");
         Assert.assertNotNull(codes);
@@ -52,7 +45,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_PerMapGroupForMultipleMapGroups() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_MultipleMapGroups.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_MultipleMapGroups.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "M");
         Assert.assertNotNull(codes);
@@ -61,7 +54,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_PerMapGroupForMultipleMapGroups_BasedOnAgeGreaterThanPredicate() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 90, "M");
         Assert.assertNotNull(codes);
@@ -70,7 +63,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_PerMapGroupForMultipleMapGroups_BasedOnAgeLesserThanPredicate() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 10, "M");
         Assert.assertNotNull(codes);
@@ -79,7 +72,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_PerMapGroupForMultipleMapGroups_BasedOnAgeRangePredicate() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Age.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 14, "M");
         Assert.assertNotNull(codes);
@@ -88,7 +81,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_ForSingleMapGroup_BasedOnMaleGenderPredicate() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "M");
         Assert.assertNotNull(codes);
@@ -97,7 +90,7 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFirstMatchingICDCodeInMapPriorityOrder_ForSingleMapGroup_BasedOnFemaleGenderPredicate() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "F");
         Assert.assertNotNull(codes);
@@ -106,33 +99,27 @@ public class Icd10EvaluatorTest {
 
     @Test
     public void shouldSelectFallbackICDCodeInMapPriorityOrder_ForSingleMapGroup() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_Gender.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "OTHER");
         Assert.assertNotNull(codes);
         Assert.assertEquals("", codes);
     }
+
 
     @Test
-    public void shouldReturnEmptyICDCodeWhenMultipleMapGroupRulesHaveEmptyMapTarget() {
-        List<ICDRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_WithEmptyMapTargets.json");
+    public void shouldReturnEmptyICDCode_PerMapGroupForMultipleMapGroups_WhenMapTargetIsEmpty() {
+        List<IcdRule> mockSortedRules = getMockMapRules("terminologyServices/icdRules_WithEmptyMapTargets.json");
         when(icd10Service.getRules(any())).thenReturn(mockSortedRules);
         String codes = icd10Evaluator.getMatchingIcdCodes("dummycode", 34, "OTHER");
         Assert.assertNotNull(codes);
         Assert.assertEquals("", codes);
     }
 
-    private List<ICDRule> getMockMapRules(String filePath) {
-        try {
-            ICDResponse icdResponse = objectMapper.readValue(readFileAsStr(filePath), ICDResponse.class);
-            return icdResponse.getItems().stream().sorted(Icd10LookupServiceImpl.customComparator).collect(Collectors.toList());
-        } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
-        }
+
+    List<IcdRule> getMockMapRules(String filePath) {
+        IcdResponse icdResponse = Icd10LookupServiceImplTest.getMockIcdResponse(filePath);
+        return icdResponse.getItems().stream().sorted(Icd10LookupServiceImpl.customComparator).collect(Collectors.toList());
     }
 
-    private String readFileAsStr(String relativePath) throws URISyntaxException, IOException {
-        Path path = Paths.get(getClass().getClassLoader().getResource(relativePath).toURI());
-        return Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.joining("\n"));
-    }
 }
