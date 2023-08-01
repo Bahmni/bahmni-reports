1b519638320a6e66a0a3e6c0c7f28b4939d53fde
Siva Reddy <sivareddy@outlook.com>
2023-08-01 18:40:24 +0530

    Update start.sh

--- package/docker/bahmni-reports/start.sh
+++ package/docker/bahmni-reports/start.sh
@@ -10,4 +10,4 @@ echo "[INFO] Running Liquibase migrations"
 sh /etc/bahmni-reports/run-liquibase.sh liquibase.xml $OPENMRS_DB_HOST $OPENMRS_DB_NAME $OPENMRS_DB_USERNAME $OPENMRS_DB_PASSWORD
 sh /etc/bahmni-reports/run-liquibase.sh liquibase_bahmni_reports.xml $REPORTS_DB_SERVER $REPORTS_DB_NAME $REPORTS_DB_USERNAME $REPORTS_DB_PASSWORD
 echo "[INFO] Starting Application"
-java $SERVER_OPTS $DEBUG_OPTS -cp "/opt/bahmni-reports/lib/extensions-1.0-SNAPSHOT-jar-with-dependencies.jar:/opt/bahmni-reports/lib/bahmni-embedded-tomcat.jar" org.bahmni.deployment.launch.Main
+java -jar $SERVER_OPTS $DEBUG_OPTS /opt/bahmni-reports/lib/bahmni-embedded-tomcat.jar
