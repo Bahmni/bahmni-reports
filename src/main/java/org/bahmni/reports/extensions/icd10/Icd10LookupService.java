2acafaf42074605d8abcf761270f7c5f895c60f4
Daviemukungi <mukungidavid@gmail.com>
2023-08-11 08:43:25 +0300

    BS-123 | icd-10 extensions outside bahmni reports (#82)
    * Removed extensions related code
    
    * Update Dockerfile
    
    * Update start.sh
    
    * Update build_publish.yml
    
    * pulled repo online and build warfile
    
    * clone git icd10 extensions repo and build the jar file
    
    * BS-123 | daviemukungi,vijay | add extensions outside repository
    
    * BS-123 | daviemukungi | Added workflows to clone ICD10 extensions repo, build the jar and include it in the execution classpath
    
    * Removed extension repo workflows in the dockerfile
    
    * updated a temporary branch to test bahmni extensions workflows
    
    * BS-123-temp | daviemukungi | Removed unused test resources and reverted back the build branch to default
    
    * BS-123 | Removed unused Dockerfile
    
    ---------
    
    Co-authored-by: vijayanandtwks <vijayanand.s@thoughtworks.com>
    Co-authored-by: Siva Reddy <sivareddy@outlook.com>

--- src/main/java/org/bahmni/reports/extensions/icd10/Icd10LookupService.java
+++
@@ -1,9 +0,0 @@
-package org.bahmni.reports.extensions.icd10;
-
-import org.bahmni.reports.extensions.icd10.bean.IcdRule;
-
-import java.util.List;
-
-public interface Icd10LookupService {
-    List<IcdRule> getRules(String snomedCode);
-}
