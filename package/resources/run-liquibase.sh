9497bcedbd0a30974cec913c2e7ed4f01597470f
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2023-09-18 12:18:09 +0530

    [Parvathy, Rahul] | BAH-3197 | Fix. MySQL Connection Error
    Co-authored-by: Parvathy Babu <parvathy.babu@thoughtworks.com>

--- package/resources/run-liquibase.sh
+++ package/resources/run-liquibase.sh
@@ -6,7 +6,7 @@ set -e -x
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=$3"
 LIQUIBASE_JAR="${WAR_DIRECTORY}/WEB-INF/lib/liquibase-core-4.8.0.jar"
 DRIVER="com.mysql.cj.jdbc.Driver"
-MYSQL_CONNECTER_JAR="${WAR_DIRECTORY}/WEB-INF/lib/mysql-connector-java-8.0.26.jar"
+MYSQL_CONNECTER_JAR="${WAR_DIRECTORY}/WEB-INF/lib/mysql-connector-java-8.0.28.jar"
 CLASSPATH="$LIQUIBASE_JAR:$MYSQL_CONNECTER_JAR"
 
 CHANGE_LOG_FILE="$1"
