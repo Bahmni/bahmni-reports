aded272ec5c7847823b7ea497c34b5f62368294f
Vivek Biswas <vivek.biswas@thoughtworks.com>
2025-06-25 11:11:00 +0530

    [WIP][Review Changes] JDK17 Migration: Fix upload API failure due to wrongly configured multipart resolver
    - Tomcat 10 requires this to be configured at initialization phase, hence moved the configuration from BahmniReportsConfiguration to Initializer
    - Hardcoding max upload size to 5MB since configuration properties bean would not be initialized in this phase

--- src/main/java/org/bahmni/reports/BahmniReportsConfiguration.java
+++ src/main/java/org/bahmni/reports/BahmniReportsConfiguration.java
@@ -7,8 +7,6 @@ import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.bahmni.reports.builder.ComboPooledDataSourceBuilder;
 import org.bahmni.webclients.AllTrustedSSLSocketFactory;
 import org.bahmni.webclients.ConnectionDetails;
@@ -25,13 +23,9 @@ import java.beans.PropertyVetoException;
 
 @Configuration
 public class BahmniReportsConfiguration {
-
-
-    private static final long DEFAULT_MAX_UPLOAD_SIZE = 5242880L;
     private static int IDLE_CONNECTION_TEST_TIME = 300; //in seconds
 
     private BahmniReportsProperties bahmniReportsProperties;
-    private static final Logger logger = LogManager.getLogger(BahmniReportsConfiguration.class);
 
     @Autowired
     public BahmniReportsConfiguration(BahmniReportsProperties bahmniReportsProperties){
@@ -124,10 +118,6 @@ public class BahmniReportsConfiguration {
 
     @Bean
     public MultipartResolver multipartResolver() {
-//        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-//        commonsMultipartResolver.setDefaultEncoding("utf-8");
-//        commonsMultipartResolver.setMaxUploadSize(getFileUploadMaxSize());
-//        return commonsMultipartResolver;
         return new StandardServletMultipartResolver();
     }
 
@@ -143,17 +133,4 @@ public class BahmniReportsConfiguration {
         dataSource.setPreferredTestQuery("SELECT 1;");
         return dataSource;
     }
-
-    private long getFileUploadMaxSize() {
-        String fileUploadMaxSize = bahmniReportsProperties.getFileUploadMaxSize();
-        if (fileUploadMaxSize != null) {
-            try {
-                return Long.valueOf(fileUploadMaxSize.trim());
-            } catch (NumberFormatException nfe) {
-                logger.error("Property for max file upload is incorrectly defined. Falling back on default 5MB");
-            }
-        }
-        return DEFAULT_MAX_UPLOAD_SIZE;
-    }
-
 }
--- src/main/java/org/bahmni/reports/web/Initializer.java
+++ src/main/java/org/bahmni/reports/web/Initializer.java
@@ -1,14 +1,19 @@
 package org.bahmni.reports.web;
 
+import jakarta.servlet.MultipartConfigElement;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration.Dynamic;
 
+import org.apache.commons.lang3.CharEncoding;
 import org.bahmni.reports.web.security.SecurityConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+
 public class Initializer implements WebApplicationInitializer {
+    private static final long DEFAULT_MAX_UPLOAD_SIZE = 5242880L;
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -18,5 +23,24 @@ public class Initializer implements WebApplicationInitializer {
         Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
         dynamic.addMapping("/");
         dynamic.setLoadOnStartup(1);
+        configureMultipartHandling(servletContext, dynamic);
+    }
+
+    /**
+     * This method is responsible for handling the multipart configurations such as max upload size and character encoding.
+     * By default, it sets the maximum upload size to 5MB and uses UTF-8 encoding for multipart files.
+     *
+     * @param servletContext the configured ServletContext
+     * @param dynamic the Dynamic servlet registration object
+     */
+    private void configureMultipartHandling(ServletContext servletContext, Dynamic dynamic) {
+        // Set max upload size
+        // Hardcoding max upload size to 5MB, this needs to be updated if more than 5MB is required in future
+        dynamic.setMultipartConfig(new MultipartConfigElement(null, DEFAULT_MAX_UPLOAD_SIZE, -1, -1));
+
+        // Set default encoding for multipart files
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter(CharEncoding.UTF_8);
+        characterEncodingFilter.setForceEncoding(true);
+        servletContext.addFilter("characterEncodingFilter", characterEncodingFilter);
     }
 }
